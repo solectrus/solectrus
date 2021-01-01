331ca6952891e1615ae09b22ad810bd109b99324
Georg Ledermann <georg@ledermann.dev>
2021-01-01 12:24:10 +0100

    Render live indicator in the header as a separate frame

--- spec/requests/stats_spec.rb
+++ spec/requests/stats_spec.rb
@@ -9,7 +9,7 @@ describe 'Stats' do
 
     context 'with params' do
       it 'renders', vcr: true do
-        get stats_path(timeframe: 'current')
+        get stats_path(timeframe: 'current'), headers: { 'ACCEPT' => 'text/html; turbo-stream, text/html, application/xhtml+xml' }
         expect(response).to have_http_status(:ok)
       end
     end
