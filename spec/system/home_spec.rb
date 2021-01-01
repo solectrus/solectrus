74a4cee90eea7bae360f8e8c0bdad35b5e291344
Georg Ledermann <georg@ledermann.dev>
2021-01-01 08:47:27 +0100

    Add timeframes 7d and 30d

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -7,5 +7,11 @@ describe 'Home', type: :system, js: true, vcr: true do
 
     click_on 'Letzte 24 Stunden'
     expect(page).to have_text('kWh', count: 4)
+
+    click_on 'Letzte 7 Tage'
+    expect(page).to have_text('kWh', count: 4)
+
+    click_on 'Letzte 30 Tage'
+    expect(page).to have_text('kWh', count: 4)
   end
 end
