a6b6a911ea8c88b718cef37be6cfbf5a4e1b5577
Georg Ledermann <georg@ledermann.dev>
2021-01-03 10:57:15 +0100

    Fix system specs to wait for updated data

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -7,18 +7,23 @@ describe 'Home', type: :system, js: true, vcr: true do
     expect(page).to have_text('%', count: 1)
 
     click_on I18n.t('calculator.day')
+    expect(page).to have_css('svg')
     expect(page).to have_text('kWh', count: 4)
 
     click_on I18n.t('calculator.week')
+    expect(page).to have_css('svg')
     expect(page).to have_text('kWh', count: 4)
 
     click_on I18n.t('calculator.month')
+    expect(page).to have_css('svg')
     expect(page).to have_text('kWh', count: 4)
 
     click_on I18n.t('calculator.year')
+    expect(page).to have_css('svg')
     expect(page).to have_text('kWh', count: 4)
 
     click_on I18n.t('calculator.all')
+    expect(page).to have_css('svg')
     expect(page).to have_text('kWh', count: 4)
   end
 end
