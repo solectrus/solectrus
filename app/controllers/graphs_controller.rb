1c9b025c7c357a229089528c1293c3a7cacf092b
Georg Ledermann <georg@ledermann.dev>
2021-01-05 10:32:15 +0100

    Add links to select different graphs

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,11 +1,15 @@
 class GraphsController < ApplicationController
   def index
-    @chart = FluxChart.new(:house_power).public_send(timeframe)
+    @chart = FluxChart.new(field.to_sym).public_send(timeframe)
   end
 
   private
 
   helper_method def timeframe
     params[:timeframe]
+  end
+
+  helper_method def field
+    params[:field]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
 
   mount Lockup::Engine, at: '/lockup' if Rails.env.production?
 
-  constraints timeframe: /now|day|week|month|year|all/ do
-    get '/stats/(:timeframe)', to: 'stats#index', as: :stats
-    get '/graphs/(:timeframe)', to: 'graphs#index', as: :graphs
-    get '/(:timeframe)', to: 'home#index', as: :root
+  constraints timeframe: /now|day|week|month|year|all/,
+              field: /inverter_power|house_power|grid_power_plus|grid_power_minus|bat_power_minus|bat_power_plus|bat_fuel_charge/ do
+    get '/stats/(:timeframe)(/:field)', to: 'stats#index', as: :stats
+    get '/graphs/(:timeframe)(/:field)', to: 'graphs#index', as: :graphs
+    get '/(:timeframe)(/:field)', to: 'home#index', as: :root
   end
 end
