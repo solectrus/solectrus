48ba1cc2460a46eba12ebfaf341350f8d3f0edf5
Georg Ledermann <georg@ledermann.dev>
2021-01-05 17:50:48 +0100

    Webpacker: Go back to v5

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-const webpackConfig = require('./base')
+const environment = require('./environment')
 
-module.exports = webpackConfig
+module.exports = environment.toWebpackConfig()
