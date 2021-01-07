7113873aa764b2be61ad493e7482226fe7f66bc5
Georg Ledermann <georg@ledermann.dev>
2021-01-07 18:24:29 +0100

    ChartKick: Use JSON endpoint

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,7 +1,11 @@
 class ChartsController < ApplicationController
   include ParamsHandling
 
-  def index; end
+  def index
+    respond_to do |format|
+      format.json { render json: chart }
+    end
+  end
 
   private
 
