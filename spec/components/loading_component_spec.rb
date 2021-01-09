21cd028bfa92e1a2ff3346bb926abb5d848a9228
Georg Ledermann <georg@ledermann.dev>
2021-01-09 07:07:38 +0100

    Build LoadingComponent

---
+++ spec/components/loading_component_spec.rb
@@ -0,0 +1,9 @@
+describe LoadingComponent, type: :component do
+  it 'renders SVG' do
+    expect(
+      render_inline(described_class.new).css('svg').to_html
+    ).to include(
+      'viewbox'
+    )
+  end
+end
