7fe5b93710936baa6b8d937fa2228ae0bf6cd912
Georg Ledermann <georg@ledermann.dev>
2021-01-10 06:42:04 +0100

    Add "tippy.js" for tooltips

---
+++ app/javascript/controllers/tippy_controller.js
@@ -0,0 +1,35 @@
+import { Controller } from 'stimulus'
+
+import tippy from 'tippy.js'
+import 'tippy.js/dist/tippy.css'
+import 'tippy.js/animations/scale.css'
+import 'tippy.js/themes/light-border.css'
+
+export default class extends Controller {
+  static targets = [ 'html' ]
+
+  connect() {
+    tippy(
+      this.element, {
+        allowHTML: true,
+        arrow: true,
+        placement: 'bottom',
+        theme: 'light-border',
+        animation: 'scale',
+        content: (reference) => {
+          const title = reference.getAttribute('title')
+
+          if (title) {
+            // Remove title from DOM element to avoid native browser tooltips
+            reference.removeAttribute('title')
+
+            return title
+          } else if (this.hasHtmlTarget)
+            return this.htmlTarget.innerHTML
+          else
+            console.warn("TippyController: Title or HTML target required!")
+        }
+      }
+    )
+  }
+}
