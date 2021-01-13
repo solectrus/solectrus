dfedbe0e142a7532e3cb357a1351cbd6ee4f1b8a
Georg Ledermann <georg@ledermann.dev>
2021-01-13 15:09:11 +0100

    Add wallbox data
    Closes #39

--- app/services/now_calculator.rb
+++ app/services/now_calculator.rb
@@ -5,6 +5,7 @@ class NowCalculator < BaseCalculator
     build_context FluxSum.new(
       :inverter_power,
       :house_power,
+      :wallbox_charge_power,
       :grid_power_plus,
       :grid_power_minus,
       :bat_power_minus,
