20b1c3c26365e796b1415a26a959830b4fc374f2
Georg Ledermann <georg@ledermann.dev>
2021-01-13 06:45:45 +0100

    :arrow_up: Upgrade @hotwire/turbo to 7.0.0-beta.3

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,3 +2,7 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+
+# TODO: Remove after updating gem "turbo-rails"
+# https://github.com/hotwired/turbo-rails/pull/75
+Mime::Type.register 'text/vnd.turbo-stream.html', :turbo_stream
