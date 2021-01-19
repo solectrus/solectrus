fe61704f5d9e087e648b79e337e130f4f9116012
Georg Ledermann <georg@ledermann.dev>
2021-01-19 10:21:45 +0100

    FlowComponent:: Dynamic height

--- app/components/flow/component.rb
+++ app/components/flow/component.rb
@@ -3,44 +3,25 @@
 # https://stackoverflow.com/questions/58660120/create-infinite-loop-with-css-keyframes
 
 class Flow::Component < ViewComponent::Base
-  def initialize(value:, max:, signal:)
+  MAXIMUM = 10_000
+
+  def initialize(value:, signal:)
     super
     @value = value.to_f
-    @max = max.to_f
     @signal = signal
   end
 
   attr_accessor :value, :signal
 
-  def height_class # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
-    if quote < 0.01
-      'h-0 border'
-    elsif quote < 0.03
-      'h-1'
-    elsif quote < 0.05
-      'h-2'
-    elsif quote < 0.1
-      'h-3'
-    elsif quote < 0.2
-      'h-5'
-    elsif quote < 0.3
-      'h-6'
-    elsif quote < 0.4
-      'h-7'
-    elsif quote < 0.5
-      'h-8'
-    elsif quote < 0.6
-      'h-9'
-    elsif quote < 0.7
-      'h-10'
-    elsif quote < 0.8
-      'h-11'
-    else
-      'h-12'
-    end
+  def quote
+    [ @value.to_f / MAXIMUM, 1 ].min
   end
 
-  def quote
-    @quote ||= [ @value / @max, 1.0 ].min
+  def ease_out_cubic
+    (quote - 1)**3 + 1
+  end
+
+  def height
+    "#{(100 * ease_out_cubic).round}%"
   end
 end
