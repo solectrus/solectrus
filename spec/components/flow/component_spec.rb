6111042ff3de6624bd32dc73f377a614c698e8c6
Georg Ledermann <georg@ledermann.dev>
2021-01-19 10:21:45 +0100

    FlowComponent:: Dynamic height

--- spec/components/flow/component_spec.rb
+++ spec/components/flow/component_spec.rb
@@ -1,51 +1,51 @@
 require 'rails_helper'
 
 describe Flow::Component, type: :component do
-  let(:component) { described_class.new(value: value, max: max, signal: true) }
+  let(:component) { described_class.new(value: value, signal: true) }
 
-  describe '#height_class' do
-    subject { component.height_class }
+  describe '#height' do
+    subject { component.height }
 
     context 'when 150%' do
-      let(:value)     { 1_500          }
-      let(:max)       { 1_000          }
+      let(:value) { 15_000 }
 
-      it { is_expected.to eq('h-12') }
+      it { is_expected.to eq('100%') }
     end
 
     context 'when 100%' do
-      let(:value)     { 1_000          }
-      let(:max)       { 1_000          }
+      let(:value) { 10_000 }
 
-      it { is_expected.to eq('h-12') }
+      it { is_expected.to eq('100%') }
     end
 
-    context 'when 50%' do
-      let(:value)     { 500            }
-      let(:max)       { 1_000          }
+    context 'when 3/4' do
+      let(:value) { 7_500 }
 
-      it { is_expected.to eq('h-9') }
+      it { is_expected.to eq('98%') }
     end
 
-    context 'when 33%' do
-      let(:value)     { 333            }
-      let(:max)       { 1_000          }
+    context 'when 1/2' do
+      let(:value) { 5_000 }
 
-      it { is_expected.to eq('h-7') }
+      it { is_expected.to eq('88%') }
+    end
+
+    context 'when 1/3' do
+      let(:value) { 3_333 }
+
+      it { is_expected.to eq('70%') }
     end
 
     context 'when 5%' do
-      let(:value)     { 50             }
-      let(:max)       { 1_000          }
+      let(:value) { 500 }
 
-      it { is_expected.to eq('h-3') }
+      it { is_expected.to eq('14%') }
     end
 
-    context 'when 0%' do
-      let(:value)     { 0              }
-      let(:max)       { 1_000          }
+    context 'when 0' do
+      let(:value) { 0 }
 
-      it { is_expected.to eq('h-0 border') }
+      it { is_expected.to eq('0%') }
     end
   end
 end
