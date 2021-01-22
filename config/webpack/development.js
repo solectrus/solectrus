46220947528c49373c34ce591c42c8f2932930d3
Georg Ledermann <georg@ledermann.dev>
2021-01-22 10:43:45 +0100

    :arrow_up: Upgrade gem "webpacker" to 6.0.0-beta.3

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,9 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const webpackConfig = require('./base')
 
-// Fix from https://github.com/rails/webpacker/issues/2832#issuecomment-762433155
-if (webpackConfig.devServer) {
-  webpackConfig.devServer.injectClient = true
-}
-
 module.exports = webpackConfig
