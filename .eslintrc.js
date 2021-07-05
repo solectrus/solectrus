11fd34b1aa0e407811a468638c78cd485f46c060
Georg Ledermann <georg@ledermann.dev>
2021-07-05 07:26:14 +0200

    Add Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  plugins: [],
+  extends: ["eslint:recommended", "prettier", "plugin:prettier/recommended"],
+  globals: {},
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+  },
+  parserOptions: {
+    sourceType: "module", // allow the use of imports statements
+    ecmaVersion: 2020, // allow the parsing of modern ecmascript
+  },
+};
