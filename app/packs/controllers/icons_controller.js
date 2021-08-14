67d25190e4f8617e3fa062aa5d7a0fa30bbbf29d
Georg Ledermann <georg@ledermann.dev>
2021-08-14 09:04:39 +0200

    Reformat JS files to match Prettier config

--- app/packs/controllers/icons_controller.js
+++ app/packs/controllers/icons_controller.js
@@ -1,28 +1,28 @@
-import { Controller } from "stimulus";
-import { config, library, dom } from "@fortawesome/fontawesome-svg-core";
+import { Controller } from 'stimulus';
+import { config, library, dom } from '@fortawesome/fontawesome-svg-core';
 
 // ------------------------- Add new icons here
-import { faChevronLeft } from "@fortawesome/free-solid-svg-icons/faChevronLeft";
-import { faChevronRight } from "@fortawesome/free-solid-svg-icons/faChevronRight";
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons/faChevronLeft';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons/faChevronRight';
 
-import { faSun } from "@fortawesome/free-solid-svg-icons/faSun";
-import { faHome } from "@fortawesome/free-solid-svg-icons/faHome";
-import { faCar } from "@fortawesome/free-solid-svg-icons/faCar";
-import { faPlug } from "@fortawesome/free-solid-svg-icons/faPlug";
+import { faSun } from '@fortawesome/free-solid-svg-icons/faSun';
+import { faHome } from '@fortawesome/free-solid-svg-icons/faHome';
+import { faCar } from '@fortawesome/free-solid-svg-icons/faCar';
+import { faPlug } from '@fortawesome/free-solid-svg-icons/faPlug';
 
-import { faBatteryEmpty } from "@fortawesome/free-solid-svg-icons/faBatteryEmpty";
-import { faBatteryQuarter } from "@fortawesome/free-solid-svg-icons/faBatteryQuarter";
-import { faBatteryHalf } from "@fortawesome/free-solid-svg-icons/faBatteryHalf";
-import { faBatteryThreeQuarters } from "@fortawesome/free-solid-svg-icons/faBatteryThreeQuarters";
-import { faBatteryFull } from "@fortawesome/free-solid-svg-icons/faBatteryFull";
+import { faBatteryEmpty } from '@fortawesome/free-solid-svg-icons/faBatteryEmpty';
+import { faBatteryQuarter } from '@fortawesome/free-solid-svg-icons/faBatteryQuarter';
+import { faBatteryHalf } from '@fortawesome/free-solid-svg-icons/faBatteryHalf';
+import { faBatteryThreeQuarters } from '@fortawesome/free-solid-svg-icons/faBatteryThreeQuarters';
+import { faBatteryFull } from '@fortawesome/free-solid-svg-icons/faBatteryFull';
 
-import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 // -------------------------
 
 export default class extends Controller {
   initialize() {
     // Fix flash of missing icons
-    config.mutateApproach = "sync";
+    config.mutateApproach = 'sync';
 
     library.add(
       faSun,
@@ -36,7 +36,7 @@ export default class extends Controller {
       faBatteryHalf,
       faBatteryThreeQuarters,
       faBatteryFull,
-      faGithub
+      faGithub,
     );
   }
 
--- app/packs/controllers/index.js
+++ app/packs/controllers/index.js
@@ -1,19 +1,19 @@
 // Load all the controllers within this directory and all subdirectories.
 // Controller files must be named *_controller.js.
 
-import { Application } from "stimulus";
-import { definitionsFromContext } from "stimulus/webpack-helpers";
+import { Application } from 'stimulus';
+import { definitionsFromContext } from 'stimulus/webpack-helpers';
 
 const application = Application.start();
-const context = require.context("controllers", true, /_controller\.js$/);
+const context = require.context('controllers', true, /_controller\.js$/);
 const contextComponents = require.context(
-  "../../components",
+  '../../components',
   true,
-  /_controller\.js$/
+  /_controller\.js$/,
 );
 
 application.load(
   definitionsFromContext(context).concat(
-    definitionsFromContext(contextComponents)
-  )
+    definitionsFromContext(contextComponents),
+  ),
 );
