88cec44878a3c11f4db132d4fa3d95401583dd82
Georg Ledermann <georg@ledermann.dev>
2021-10-09 06:29:32 +0200

    Update view_component sidecar config

--- app/packs/components.js
+++ app/packs/components.js
@@ -2,4 +2,4 @@ function importAll(r) {
   r.keys().forEach(r);
 }
 
-importAll(require.context('../components', true, /component.js$/));
+importAll(require.context('../components', true, /[_/]component\.js$/));
