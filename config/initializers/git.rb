7e96bd46076b9087cf83d5f5b269b41c0e88184a
Georg Ledermann <georg@ledermann.dev>
2021-11-21 06:41:27 +0100

    Don't fail if there are no git tags

--- config/initializers/git.rb
+++ config/initializers/git.rb
@@ -1,5 +1,5 @@
 Rails.configuration.x.git.commit_version =
-  ENV.fetch('COMMIT_VERSION') { `git describe`.chomp }
+  ENV.fetch('COMMIT_VERSION') { `git describe --always`.chomp }
 
 Rails.configuration.x.git.commit_time =
   Time.zone.parse(ENV.fetch('COMMIT_TIME') { `git show -s --format=%cI` })
