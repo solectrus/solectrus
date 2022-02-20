2bcb9e56e7fd0c3d51fd715c2906b613b3d67cc7
Georg Ledermann <georg@ledermann.dev>
2022-02-20 07:46:23 +0100

    Add autarky charts
    Belongs to #663

--- app/models/senec.rb
+++ app/models/senec.rb
@@ -10,10 +10,13 @@ module Senec
     wallbox_charge_power
   ].freeze
 
-  # All fields related to power (= all but bat_fuel_charge)
+  CALCULATED_FIELDS = %w[autarky].freeze
+
+  # All fields related to power (= all but `bat_fuel_charge`)
   POWER_FIELDS = FIELDS.select { |field| field.include?('power') }.freeze
 
   # Combine plus/minus fields, e.g. `grid_power` instead of `grid_power_plus` and `grid_power_minus`
   FIELDS_COMBINED =
-    FIELDS.map { |field| field.gsub(/_plus|_minus/, '') }.uniq.freeze
+    (CALCULATED_FIELDS + FIELDS.map { |field| field.gsub(/_plus|_minus/, '') })
+      .uniq.freeze
 end
