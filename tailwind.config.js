cabcfc04a94efbb5f338c58682f96f88524ce6a6
Georg Ledermann <georg@ledermann.dev>
2022-06-10 11:16:49 +0200

    Simplify Tailwind CSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 const colors = require('tailwindcss/colors');
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   theme: {
     extend: {
@@ -11,13 +12,7 @@ module.exports = {
     },
   },
 
-  content: [
-    './app/**/*.html',
-    './app/**/*.html.erb',
-    './app/**/*.html.slim',
-    './app/**/*.rb',
-    './app/javascript/**/*.js',
-  ],
+  content: ['./app/**/*.{html.erb,html.slim,rb}', './app/javascript/**/*.js'],
 
   plugins: [
     require('@tailwindcss/typography'),
