bb1fcbf6828f663af0baac16e069a031ff54c8a8
Georg Ledermann <georg@ledermann.dev>
2022-06-20 11:09:04 +0200

    ChartLoader refactoring

--- app/components/chart_loader/component_controller.js
+++ app/components/chart_loader/component_controller.js
@@ -38,60 +38,74 @@ export default class extends Controller {
   };
 
   connect() {
-    const that = this;
-    fetch(this.urlValue)
-      .then((response) => response.json())
-      .then((data) => {
-        const options = this.optionsValue;
-
-        // I18n
-        options.scales.x.adapters = {
-          date: {
-            locale: de,
-          },
-        };
-
-        // Format numbers on y-axis
-        options.scales.y.ticks.callback = (value) =>
-          that.formattedNumber(value);
-
-        const min = that.minOf(data);
-        if (min < 0) {
-          // Disable auto-scaling if there are negative values
-          options.scales.y.max = that.maxOf(data);
-          options.scales.y.min = min;
-
-          // Draw x-axis in black
-          options.scales.y.grid = {
-            color: (context) => {
-              if (context.tick.value === 0) return '#000';
-            },
-          };
-        } else {
-          options.scales.y.min = 0;
-        }
-
-        // Format numbers in tooltips
-        options.plugins.tooltip.callbacks = {
-          label: (context) =>
-            context.dataset.label +
-            ': ' +
-            that.formattedNumber(context.parsed.y),
-        };
-
-        this.chart = new Chart(this.element, {
-          type: this.typeValue,
-          data,
-          options,
-        });
-      })
-      .catch((err) => {
-        console.warn(err);
-      });
+    this.process();
   }
 
   disconnect() {
     if (this.chart) this.chart.destroy();
+  }
+
+  async process() {
+    const data = await this.loadData();
+    const options = this.optionsValue;
+
+    // I18n
+    options.scales.x.adapters = {
+      date: {
+        locale: de,
+      },
+    };
+
+    // Format numbers on y-axis
+    options.scales.y.ticks.callback = (value) => this.formattedNumber(value);
+
+    const min = this.minOf(data);
+    if (min < 0) {
+      // Disable auto-scaling if there are negative values
+      options.scales.y.max = this.maxOf(data);
+      options.scales.y.min = min;
+
+      // Draw x-axis in black
+      options.scales.y.grid = {
+        color: (context) => {
+          if (context.tick.value === 0) return '#000';
+        },
+      };
+    } else {
+      options.scales.y.min = 0;
+    }
+
+    // Format numbers in tooltips
+    options.plugins.tooltip.callbacks = {
+      label: (context) =>
+        context.dataset.label + ': ' + this.formattedNumber(context.parsed.y),
+    };
+
+    this.chart = new Chart(this.element, {
+      type: this.typeValue,
+      data,
+      options,
+    });
+  }
+
+  async loadData() {
+    try {
+      const response = await fetch(this.urlValue, {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      return result;
+    } catch (err) {
+      console.warn(err);
+    }
   }
 
   formattedNumber(number) {
