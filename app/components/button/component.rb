78ac593601ab581788d9f08fba256ef5a62ac183
Georg Ledermann <georg@ledermann.dev>
2022-08-01 10:50:08 +0200

    Add Button component

---
+++ app/components/button/component.rb
@@ -0,0 +1,90 @@
+class Button::Component < ViewComponent::Base
+  # rubocop:disable Style/MultilineMethodSignature, Metrics/ParameterLists
+  def initialize(
+    path: nil,
+    title: nil,
+    icon: nil,
+    type: :link,
+    style: :primary,
+    options: {}
+  )
+    # rubocop:enable Style/MultilineMethodSignature, Metrics/ParameterLists
+    super
+
+    if title.nil? && icon.nil?
+      # :nocov:
+      raise ArgumentError,
+            'You must provide either a title or an icon (or both)'
+      # :nocov:
+    end
+
+    @path = path
+    @title = title
+    @icon = icon
+    @type = type
+    @style = style
+    @options = options
+  end
+
+  attr_reader :path, :title, :icon, :style, :options
+
+  def method
+    case @type
+    when :link
+      :link_to
+    when :button
+      :button_to
+    when :submit
+      :button_tag
+    else
+      # :nocov:
+      raise ArgumentError, 'Type must be :link, :button or :submit'
+      # :nocov:
+    end
+  end
+
+  def btn_class
+    %w[
+      inline-flex
+      items-center
+      py-2
+      border
+      border-transparent
+      rounded
+      focus:outline-none
+      focus:ring-2
+      focus:ring-offset-2
+      focus:ring-indigo-500
+      transition
+    ] +
+      case style
+      when :primary
+        btn_primary_class
+      when :secondary
+        btn_secondary_class
+      end
+  end
+
+  def btn_primary_class
+    %w[
+      text-white
+      bg-indigo-600
+      hover:bg-indigo-700
+      shadow-sm
+      px-3
+      hover:scale-105
+    ]
+  end
+
+  def btn_secondary_class
+    %w[hover:scale-125]
+  end
+
+  def icon_name
+    icon.is_a?(Hash) ? icon.fetch(:name) : icon
+  end
+
+  def icon_class
+    "fa fa-#{icon_name} w-8 h-8"
+  end
+end
