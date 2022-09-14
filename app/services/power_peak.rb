37e44b687d02f2afa161738c42604ff8a946297d
Georg Ledermann <georg@ledermann.dev>
2022-09-14 09:57:05 +0200

    Add PowerPeak class

---
+++ app/services/power_peak.rb
@@ -0,0 +1,21 @@
+class PowerPeak < Flux::Reader
+  def result(start:, stop: nil)
+    raw = query <<-QUERY, cache_options: { expires_in: 24.hours }
+      #{from_bucket}
+      |> #{range(start:, stop:)}
+      |> #{measurements_filter}
+      |> #{fields_filter}
+      |> aggregateWindow(every: 30s, fn: mean)
+      |> max()
+    QUERY
+
+    array = raw.values
+    array.map!(&:records)
+    array.map!(&:first)
+    array.map!(&:values)
+
+    array.reduce({}) do |total, current|
+      total.merge(current['_field'] => current['_value'].round)
+    end
+  end
+end
