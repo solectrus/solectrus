70601baa83cf258a2d278e09e0b0468a9179ab63
Georg Ledermann <georg@ledermann.dev>
2022-11-14 11:19:17 +0100

    Add custom error pages

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,10 @@
+class ErrorsController < ApplicationController
+  def show
+    exception = request.env['action_dispatch.exception']
+    @status_code =
+      exception.try(:status_code) ||
+        ActionDispatch::ExceptionWrapper.new(request.env, exception).status_code
+
+    render 'show', status: @status_code
+  end
+end
