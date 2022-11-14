e3f3d0f23da13436d8338827a1035cc54cacb263
Georg Ledermann <georg@ledermann.dev>
2022-11-14 11:19:17 +0100

    Add custom error pages

---
+++ spec/requests/error_pages_spec.rb
@@ -0,0 +1,26 @@
+describe 'Error pages', vcr: { cassette_name: 'github' } do
+  describe 'error 404' do
+    it 'renders a custom 404 error page' do
+      without_detailed_exceptions { get '/non-existing-route' }
+
+      expect(response).to have_http_status(:not_found)
+      expect(response.body).to include(I18n.t('errors.404.title'))
+    end
+  end
+
+  describe 'error 500' do
+    let(:failing_controller) { instance_double(HomeController) }
+
+    before do
+      allow(HomeController).to receive(:new).and_return(failing_controller)
+      allow(failing_controller).to receive(:index).and_raise('fail')
+    end
+
+    it 'renders a custom 500 error page' do
+      without_detailed_exceptions { get '/' }
+
+      expect(response).to have_http_status(:internal_server_error)
+      expect(response.body).to include(I18n.t('errors.500.title'))
+    end
+  end
+end
---
+++ spec/support/exceptions.rb
@@ -0,0 +1,18 @@
+module WithoutDetailedExceptions
+  RSpec.configure { |config| config.include self, type: :request }
+
+  def without_detailed_exceptions
+    env_config = Rails.application.env_config
+    original_show_exceptions = env_config['action_dispatch.show_exceptions']
+    original_show_detailed_exceptions =
+      env_config['action_dispatch.show_detailed_exceptions']
+    env_config['action_dispatch.show_exceptions'] = true
+    env_config['action_dispatch.show_detailed_exceptions'] = false
+    yield
+  ensure
+    env_config['action_dispatch.show_exceptions'] = original_show_exceptions
+    env_config[
+      'action_dispatch.show_detailed_exceptions'
+    ] = original_show_detailed_exceptions
+  end
+end
