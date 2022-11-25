25311723043cde846d686e0964c96c6fd41be950
Georg Ledermann <georg@ledermann.dev>
2022-11-25 06:39:07 +0100

    Scale: Catch exception (and log debug info)
    Belongs to #1489

--- app/services/scale.rb
+++ app/services/scale.rb
@@ -11,6 +11,9 @@ class Scale
 
     target.first +
       (extent * (Math.log(value)**factor) / (Math.log(max)**factor)).round
+  rescue StandardError => e
+    Rails.logger.info "WARNING: Invalid input, cannot scale: value: #{value}, max: #{max}, error: #{e}"
+    0
   end
 
   private
--- spec/services/scale_spec.rb
+++ spec/services/scale_spec.rb
@@ -15,20 +15,32 @@ describe Scale do
     it { expect_result(10_000, 100) }
   end
 
-  context 'when initialized with a nil max value' do
+  context 'when initialized with max value of NIL' do
     let(:max) { nil }
 
     it { expect_result(0, 0) }
     it { expect_result(1000, 0) }
-    it { expect_result(2000, 0) }
-    it { expect_result(3000, 0) }
-    it { expect_result(4000, 0) }
-    it { expect_result(5000, 0) }
-    it { expect_result(6000, 0) }
-    it { expect_result(7000, 0) }
-    it { expect_result(8000, 0) }
-    it { expect_result(9000, 0) }
     it { expect_result(10_000, 0) }
+  end
+
+  context 'when initialized with a max value of 0' do
+    let(:max) { 0 }
+
+    it { expect_result(0, 0) }
+    it { expect_result(1000, 0) }
+    it { expect_result(10_000, 0) }
+  end
+
+  context 'when exception is raised' do
+    before { allow(Math).to receive(:log).and_raise(StandardError) }
+
+    let(:max) { 42 }
+
+    it 'catches exception and returns 0' do
+      allow(Rails.logger).to receive(:info)
+      expect_result(100, 0)
+      expect(Rails.logger).to have_received(:info).with(/Invalid input/).once
+    end
   end
 
   private
