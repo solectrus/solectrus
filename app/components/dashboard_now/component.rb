c4c47bf31abd1356e31af1f9a7919130702cbc2a
Georg Ledermann <georg@ledermann.dev>
2022-11-26 18:50:57 +0100

    Revert "Add peak logging"
    This reverts commit 86079bb1e75563ff75be919a783278463c2c18ac.
    
    Belongs to #1496

--- app/components/dashboard_now/component.rb
+++ app/components/dashboard_now/component.rb
@@ -17,15 +17,9 @@ class DashboardNow::Component < ViewComponent::Base
 
   def peak
     @peak ||=
-      begin
-        result =
-          PowerPeak.new(
-            fields: Senec::POWER_FIELDS,
-            measurements: %w[SENEC],
-          ).result(start: 30.days.ago.beginning_of_day)
-        Rails.logger.info "Peak: #{result.inspect}"
-
-        result
-      end
+      PowerPeak.new(
+        fields: Senec::POWER_FIELDS,
+        measurements: %w[SENEC],
+      ).result(start: 30.days.ago.beginning_of_day)
   end
 end
