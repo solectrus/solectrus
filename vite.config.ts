38669efe0a7b82da6b634afcc98e18a348113388
Georg Ledermann <georg@ledermann.dev>
2022-12-05 19:05:39 +0100

    Vite: Use "vite-plugin-rails" to simplify config

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,20 @@
 import { defineConfig } from 'vite';
 import { splitVendorChunkPlugin } from 'vite';
-import RubyPlugin from 'vite-plugin-ruby';
-import FullReload from 'vite-plugin-full-reload';
-import StimulusHMR from 'vite-plugin-stimulus-hmr';
-import { brotliCompressSync } from 'zlib';
-import gzipPlugin from 'rollup-plugin-gzip';
+import ViteRails from 'vite-plugin-rails';
 import { fileURLToPath, URL } from 'url';
 
 export default defineConfig({
   plugins: [
     splitVendorChunkPlugin(),
-    RubyPlugin(),
-    StimulusHMR(),
-    FullReload([
-      'config/routes.rb',
-      'app/views/**/*',
-      'app/components/**/*',
-      'config/locales/**/*.yml',
-    ]),
-    // Create gzip copies of relevant assets
-    gzipPlugin(),
-    // Create brotli copies of relevant assets
-    gzipPlugin({
-      customCompression: (content) => brotliCompressSync(Buffer.from(content)),
-      fileName: '.br',
+    ViteRails({
+      fullReload: {
+        additionalPaths: [
+          'config/routes.rb',
+          'app/views/**/*',
+          'app/components/**/*',
+          'config/locales/**/*.yml',
+        ],
+      },
     }),
   ],
   resolve: {
