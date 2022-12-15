e7903f609574e4bcc0a77bbc11f58fc5368d5988
Georg Ledermann <georg@ledermann.dev>
2022-12-15 19:37:11 +0100

    Tippy: Don't throw exception if there is no content

--- app/javascript/controllers/tippy_controller.ts
+++ app/javascript/controllers/tippy_controller.ts
@@ -21,27 +21,22 @@ export default class extends Controller {
   private instance: Instance | undefined;
 
   connect() {
+    const content =
+      (this.hasHtmlTarget && this.htmlTarget.innerHTML) ||
+      this.element.getAttribute('title');
+    if (!content) return;
+
     this.instance = tippy(this.element, {
+      content,
       allowHTML: true,
       arrow: true,
       placement: this.placementValue,
       theme: 'light-border',
       animation: 'scale',
-      content: (reference): string => {
-        const title = reference.getAttribute('title');
-
-        if (title) {
-          // Remove title from DOM element to avoid native browser tooltips
-          reference.removeAttribute('title');
-
-          return title;
-        }
-
-        if (this.hasHtmlTarget) return this.htmlTarget.innerHTML;
-
-        throw new Error('Tippy: No title or html target found!');
-      },
     });
+
+    // Remove title from DOM element to avoid native browser tooltips
+    this.element.removeAttribute('title');
   }
 
   disconnect() {
