71ab43d886f52ad7b59e73713026445a3060913b
Georg Ledermann <georg@ledermann.dev>
2022-12-24 07:37:03 +0100

    Update check: Limit timeout, ignore errors, cleanup

--- app/services/version.rb
+++ app/services/version.rb
@@ -1,23 +1,43 @@
 class Version
+  CHECK_URL = 'https://update.solectrus.de'.freeze
+  public_constant :CHECK_URL
+
   def self.latest
     new.latest
   end
 
   def latest
-    uri = URI('https://update.solectrus.de')
+    uri = URI(CHECK_URL)
     response =
-      Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+      Net::HTTP.start(
+        uri.host,
+        uri.port,
+        use_ssl: true,
+        open_timeout: 3,
+        read_timeout: 5,
+      ) do |http|
         request = Net::HTTP::Get.new(uri.request_uri)
         request.initialize_http_header(
           'Accept' => 'application/json',
           'User-Agent' => 'Solectrus',
           'X-Version' => Rails.configuration.x.git.commit_version,
-          'Referer' => Rails.configuration.x.app_host,
+          'Referer' => app_url,
         )
 
         http.request(request)
       end
 
     JSON.parse(response.body)['version'] if response.is_a?(Net::HTTPSuccess)
+  rescue StandardError
+    # Mainly ignore timeout errors, but other errors must not throw an exception
+    nil
+  end
+
+  private
+
+  def app_url
+    return unless Rails.configuration.x.app_host
+
+    "#{Rails.configuration.x.force_ssl ? 'https' : 'http'}://#{Rails.configuration.x.app_host}"
   end
 end
--- spec/services/version_spec.rb
+++ spec/services/version_spec.rb
@@ -1,8 +1,8 @@
 describe Version do
-  subject(:api) { described_class.new }
+  subject(:version) { described_class }
 
-  describe '#latest_release' do
-    subject { api.latest }
+  describe '.latest' do
+    subject { version.latest }
 
     context 'when the request succeeds', vcr: { cassette_name: 'version' } do
       it { is_expected.to eq('v0.7.4') }
@@ -10,10 +10,22 @@ describe Version do
 
     context 'when the request fails' do
       before do
-        stub_request(:get, 'https://update.solectrus.de').to_return(
+        stub_request(:get, Version::CHECK_URL).to_return(
           status: [500, 'Internal Server Error'],
         )
       end
+
+      it { is_expected.to be_nil }
+    end
+
+    context 'when the request timeouts' do
+      before { stub_request(:get, Version::CHECK_URL).to_timeout }
+
+      it { is_expected.to be_nil }
+    end
+
+    context 'when response is invalid', vcr: { cassette_name: 'version' } do
+      before { allow(JSON).to receive(:parse).and_raise(JSON::ParserError) }
 
       it { is_expected.to be_nil }
     end
