12eaeb02f1634c4abceac84023d179df214d2de1
Georg Ledermann <georg@ledermann.dev>
2023-02-09 07:13:18 +0100

    Fix RuboCop warning

--- app/models/senec.rb
+++ app/models/senec.rb
@@ -21,7 +21,7 @@ module Senec
   # Combine plus/minus fields, e.g. `grid_power` instead of `grid_power_plus` and `grid_power_minus`
   FIELDS_COMBINED =
     (
-      CALCULATED_FIELDS + FIELDS.map { |field| field.gsub(/_plus|_minus/, '') }
+      FIELDS.map { |field| field.gsub(/_plus|_minus/, '') } + CALCULATED_FIELDS
     ).uniq.freeze
   public_constant :FIELDS_COMBINED
 end
