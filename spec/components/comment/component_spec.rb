009ea8b29968a5a5ad86584af0acbd711b0e50f9
Georg Ledermann <georg@ledermann.dev>
2023-03-05 14:57:36 +0100

    Forecast comment: Don't render tooltip before sunset
    Belongs to #1766

--- spec/components/comment/component_spec.rb
+++ spec/components/comment/component_spec.rb
@@ -25,6 +25,10 @@ describe Comment::Component, type: :component do
       it 'comments correctly' do
         expect(page).to have_text I18n.t('forecast.exactly')
       end
+
+      it 'has no tippy-tooltip' do
+        expect(page.native.inner_html).not_to include('<template')
+      end
     end
 
     context 'when deviation is positive' do
@@ -36,6 +40,10 @@ describe Comment::Component, type: :component do
                     percent: '10 %',
                   )
       end
+
+      it 'has tippy-tooltip' do
+        expect(page.native.inner_html).to include('<template')
+      end
     end
 
     context 'when deviation is negative' do
@@ -43,6 +51,10 @@ describe Comment::Component, type: :component do
 
       it 'comments correctly' do
         expect(page).to have_text I18n.t('forecast.worse_html', percent: '10 %')
+      end
+
+      it 'has tippy-tooltip' do
+        expect(page.native.inner_html).to include('<template')
       end
     end
   end
@@ -57,6 +69,10 @@ describe Comment::Component, type: :component do
       it 'comments correctly' do
         expect(page).to have_text I18n.t('forecast.expect_html', value: '1 kWh')
       end
+
+      it 'has no tippy-tooltip' do
+        expect(page.native.inner_html).not_to include('<template')
+      end
     end
   end
 
@@ -69,6 +85,10 @@ describe Comment::Component, type: :component do
 
       it 'comments correctly' do
         expect(page).to have_text I18n.t('forecast.worse_html', percent: '10 %')
+      end
+
+      it 'has tippy-tooltip' do
+        expect(page.native.inner_html).to include('<template')
       end
     end
   end
