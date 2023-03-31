d95ca0c4b2f73bcc9106eefa6d9098c4d9638fe7
Georg Ledermann <georg@ledermann.dev>
2023-03-31 09:46:59 +0200

    Native Select: Add hack to set minimal width

--- app/javascript/controllers/select_controller.ts
+++ app/javascript/controllers/select_controller.ts
@@ -2,7 +2,33 @@ import { Controller } from '@hotwired/stimulus';
 import * as Turbo from '@hotwired/turbo';
 
 export default class extends Controller<HTMLSelectElement> {
+  static targets = ['select', 'temp'];
+
+  declare readonly selectTarget: HTMLSelectElement;
+  declare readonly tempTarget: HTMLSelectElement;
+
+  connect() {
+    this.autoWidth();
+  }
+
   onChange() {
-    Turbo.visit(this.element.value);
+    Turbo.visit(this.selectTarget.value);
+  }
+
+  autoWidth() {
+    this.selectTarget.style.width = `${this.widthOfSelectedOption}px`;
+  }
+
+  // Hack to get the width of the selected option
+  get widthOfSelectedOption() {
+    // Get the text of the selected option
+    const text =
+      this.selectTarget.options[this.selectTarget.selectedIndex].text;
+
+    // Use a temporary select which has just ONE option - the selected one
+    this.tempTarget.innerHTML = `<option selected>${text}</option>`;
+
+    // Return the width of the temporary select
+    return this.tempTarget.clientWidth;
   }
 }
