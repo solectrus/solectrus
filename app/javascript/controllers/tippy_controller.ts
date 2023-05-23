bba39f96bed984cccc22b554de16b90964b20a61
Georg Ledermann <georg@ledermann.dev>
2023-05-23 12:11:23 +0200

    On touch devices, first click shows the tooltip
    Belongs to #2018

--- app/javascript/controllers/tippy_controller.ts
+++ app/javascript/controllers/tippy_controller.ts
@@ -19,6 +19,7 @@ export default class extends Controller {
   declare readonly htmlTargets: HTMLElement[];
 
   private instance: Instance | undefined;
+  private onClick: ((event: Event) => void) | undefined;
 
   connect() {
     const title = this.element.getAttribute('title');
@@ -32,7 +33,6 @@ export default class extends Controller {
       placement: this.placementValue,
       theme: 'light-border',
       animation: 'scale',
-      touch: 'hold',
     });
 
     // Remove title from DOM element to avoid native browser tooltips
@@ -40,9 +40,33 @@ export default class extends Controller {
 
     // Set aria-label to keep a discernible text
     this.element.ariaLabel = title;
+
+    // Add click listener
+    this.onClick = this.clickHandler();
+    this.element.addEventListener('click', this.onClick);
   }
 
   disconnect() {
     if (this.instance) this.instance.destroy();
+
+    // Remove click listener
+    if (this.onClick) this.element.removeEventListener('click', this.onClick);
+  }
+
+  private clickHandler() {
+    let clicks = 0;
+
+    return (event: Event) => {
+      clicks++;
+
+      if (clicks === 2 || !tippy.currentInput.isTouch) {
+        // Second click, let the default event handler do its thing (and reset counter)
+        // (Same if it's not a touch device)
+        clicks = 0;
+      } else {
+        // First click, prevent default action, so the tooltip stays open
+        event.preventDefault();
+      }
+    };
   }
 }
