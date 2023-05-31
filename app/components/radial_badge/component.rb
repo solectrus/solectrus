4ad5e9852d0899d4111d5e2fe8a657c51608b872
Georg Ledermann <georg@ledermann.dev>
2023-05-31 20:13:28 +0200

    Add info-icon to costs & savings badges
    Belongs to #1891

--- app/components/radial_badge/component.rb
+++ app/components/radial_badge/component.rb
@@ -1,5 +1,5 @@
 class RadialBadge::Component < ViewComponent::Base
-  def initialize(percent: nil, title: nil, neutral: false)
+  def initialize(percent: nil, title: nil, neutral: false, info_link: nil)
     if percent && (percent.negative? || percent > 100)
       raise ArgumentError, 'percent must be between 0 and 100'
     end
@@ -8,8 +8,9 @@ class RadialBadge::Component < ViewComponent::Base
     @percent = percent&.round
     @title = title
     @neutral = neutral || !percent
+    @info_link = info_link
   end
-  attr_reader :percent, :title, :neutral
+  attr_reader :percent, :title, :neutral, :info_link
 
   def variant_class
     'percent' if percent&.nonzero?
