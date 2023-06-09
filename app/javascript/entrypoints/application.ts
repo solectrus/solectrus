e0c3c892abb0bcce5d11afc05efb91c00eae99d8
Georg Ledermann <georg@ledermann.dev>
2023-06-09 05:30:51 +0200

    Vite: Move CSS to its own entrypoint

--- app/javascript/entrypoints/application.ts
+++ app/javascript/entrypoints/application.ts
@@ -3,5 +3,3 @@ import.meta.glob('../channels/**/*_channel.{js,ts}', { eager: true });
 
 import '../utils/setupHoneyBadger';
 import '../utils/setupStimulus';
-
-import '../stylesheets/application.css';
