50ca5db1cd768f2b38896259f6236343092b56c8
Georg Ledermann <georg@ledermann.dev>
2023-06-22 08:03:34 +0200

    Prevent right-click on touch devices
    Belongs to #2084

--- app/javascript/entrypoints/application.ts
+++ app/javascript/entrypoints/application.ts
@@ -3,3 +3,13 @@ import.meta.glob('../channels/**/*_channel.{js,ts}', { eager: true });
 
 import '@/utils/setupHoneyBadger';
 import '@/utils/setupStimulus';
+
+// Prevent right-click on touch devices
+window.onload = () => {
+  if ('ontouchstart' in window) {
+    document.addEventListener('contextmenu', (e) => {
+      e.preventDefault();
+      return false;
+    });
+  }
+};
