c2aed22d694464ab672926a941fe425dc0ede789
Georg Ledermann <georg@ledermann.dev>
2023-07-02 16:51:46 +0200

    Fix and improve dropdown styles
    Belongs to #2064

--- app/components/dropdown/component.rb
+++ app/components/dropdown/component.rb
@@ -1,13 +1,26 @@
 class Dropdown::Component < ViewComponent::Base
-  def initialize(items:, selected:)
+  def initialize(items:, selected:, on_dark: false)
     super
     @items = items
     @selected = selected
+    @on_dark = on_dark
   end
 
-  attr_reader :items, :selected
+  attr_reader :items, :selected, :on_dark
 
   def selected_item
     @selected_item ||= items.find { |item| item[:field] == selected }
+  end
+
+  def button_class
+    if on_dark
+      'text-gray-300 hover:bg-indigo-500 hover:bg-opacity-75 '
+    else
+      'text-gray-700 hover:bg-gray-50'
+    end
+  end
+
+  def select_class
+    on_dark ? 'bg-transparent text-gray-300' : 'text-gray-700'
   end
 end
