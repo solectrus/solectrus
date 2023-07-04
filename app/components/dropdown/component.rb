f4a31168a0a1b5eb69d8e5ed2e3b25ed5f41905e
Georg Ledermann <georg@ledermann.dev>
2023-07-04 21:22:23 +0200

    Tweak subnav styling

--- app/components/dropdown/component.rb
+++ app/components/dropdown/component.rb
@@ -1,26 +1,34 @@
 class Dropdown::Component < ViewComponent::Base
-  def initialize(items:, selected:, on_dark: false)
+  def initialize(items:, selected:, on_dark: false, small: false)
     super
     @items = items
     @selected = selected
     @on_dark = on_dark
+    @small = small
   end
 
-  attr_reader :items, :selected, :on_dark
+  attr_reader :items, :selected, :on_dark, :small
 
   def selected_item
     @selected_item ||= items.find { |item| item[:field] == selected }
   end
 
+  def font_size_class
+    small ? 'text-sm' : 'text-base'
+  end
+
   def button_class
     if on_dark
-      'text-gray-300 hover:bg-indigo-500 hover:bg-opacity-75 '
+      ['text-gray-300 hover:bg-indigo-500 hover:bg-opacity-75', font_size_class]
     else
-      'text-gray-700 hover:bg-gray-50'
+      ['text-gray-700 hover:bg-gray-50', font_size_class]
     end
   end
 
   def select_class
-    on_dark ? 'bg-transparent text-gray-300' : 'text-gray-700'
+    [
+      on_dark ? 'bg-transparent text-gray-300' : 'text-gray-700',
+      font_size_class,
+    ]
   end
 end
