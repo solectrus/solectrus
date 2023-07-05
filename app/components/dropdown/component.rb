3d289bf7bb09ef6a86fd378ef9ca3c7f2dbbc191
Georg Ledermann <georg@ledermann.dev>
2023-07-05 11:21:25 +0200

    Top10: Simplify subnav

--- app/components/dropdown/component.rb
+++ app/components/dropdown/component.rb
@@ -1,34 +1,14 @@
 class Dropdown::Component < ViewComponent::Base
-  def initialize(items:, selected:, on_dark: false, small: false)
+  def initialize(items:, selected:, button_class: 'bg-gray-200 hover:bg-white')
     super
     @items = items
     @selected = selected
-    @on_dark = on_dark
-    @small = small
+    @button_class = button_class
   end
 
-  attr_reader :items, :selected, :on_dark, :small
+  attr_reader :items, :selected, :button_class
 
   def selected_item
     @selected_item ||= items.find { |item| item[:field] == selected }
-  end
-
-  def font_size_class
-    small ? 'text-sm' : 'text-base'
-  end
-
-  def button_class
-    if on_dark
-      ['text-gray-300 hover:bg-indigo-500 hover:bg-opacity-75', font_size_class]
-    else
-      ['text-gray-700 hover:bg-gray-50', font_size_class]
-    end
-  end
-
-  def select_class
-    [
-      on_dark ? 'bg-transparent text-gray-300' : 'text-gray-700',
-      font_size_class,
-    ]
   end
 end
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,5 @@
 import type { Config } from 'tailwindcss';
 import colors from 'tailwindcss/colors';
-import plugin from 'tailwindcss/plugin';
-import svgToDataUri from 'mini-svg-data-uri';
 
 export default {
   content: ['./app/**/*.{slim,rb}', './app/javascript/**/*.{js,ts}'],
@@ -38,19 +36,5 @@ export default {
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
-    plugin(function ({ addComponents, theme }) {
-      // Override select's caret color as it can't be customized in @tailwindcss/forms
-      // see https://github.com/tailwindlabs/tailwindcss-forms/issues/17
-      addComponents({
-        select: {
-          'background-image': `url("${svgToDataUri(
-            `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20"><path stroke="${theme(
-              'colors.gray.400',
-              colors.gray[400],
-            )}" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6 8l4 4 4-4"/></svg>`,
-          )}")`,
-        },
-      });
-    }),
   ],
 } satisfies Config;
