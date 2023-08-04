fca3eb82efc7c46c5636b25c3f7a02f40835e286
Georg Ledermann <georg@ledermann.dev>
2023-08-04 13:44:41 +0200

    Disable logging in test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -55,4 +55,8 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # Disable logging to speed up tests
+  config.logger = Logger.new(nil)
+  config.log_level = :fatal
 end
