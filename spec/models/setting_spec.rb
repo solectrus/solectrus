e21b85e8a569da3ab830a3c8115805f35e0b8ce2
Georg Ledermann <georg@ledermann.dev>
2023-08-16 11:27:47 +0200

    Update annotatations

--- spec/models/setting_spec.rb
+++ spec/models/setting_spec.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: settings
+#
+#  id         :bigint           not null, primary key
+#  value      :text
+#  var        :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_settings_on_var  (var) UNIQUE
+#
 describe Setting do
   describe 'seed!' do
     subject(:seed!) { described_class.seed! }
