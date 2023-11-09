a1d6c9226082c7503808493636209926f7771044
Georg Ledermann <georg@ledermann.dev>
2023-11-09 11:26:51 +0100

    InfluxDB: Improve query cache, by caching "latest", too

--- app/services/power_sum.rb
+++ app/services/power_sum.rb
@@ -3,7 +3,7 @@ class PowerSum < Flux::Reader
     @timeframe = timeframe
 
     if timeframe.id == :now
-      last(1.hour.ago)
+      last(1.hour.ago.beginning_of_hour)
     else
       sum(timeframe:)
     end
@@ -99,6 +99,7 @@ class PowerSum < Flux::Reader
 
   # Cache expires depends on the timeframe
   DEFAULT_CACHE_EXPIRES = {
+    now: 5.seconds,
     day: 1.minute,
     week: 5.minutes,
     month: 10.minutes,
@@ -111,6 +112,11 @@ class PowerSum < Flux::Reader
   def default_cache_options
     return unless @timeframe
 
-    { expires_in: DEFAULT_CACHE_EXPIRES[@timeframe.id] }
+    expires_in = [
+      DEFAULT_CACHE_EXPIRES[@timeframe.id],
+      Rails.configuration.x.influx.poll_interval.seconds,
+    ].max
+
+    { expires_in: }
   end
 end
