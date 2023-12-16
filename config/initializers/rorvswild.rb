d74c13df332ab7daa27628a4384ccc9d6ec7fe95
Georg Ledermann <georg@ledermann.dev>
2023-12-16 10:54:32 +0100

    RorVsWild: Add deployment config

--- config/initializers/rorvswild.rb
+++ config/initializers/rorvswild.rb
@@ -2,5 +2,8 @@ if Rails.configuration.x.rorvswild.api_key
   RorVsWild.start(
     api_key: Rails.configuration.x.rorvswild.api_key,
     ignored_exceptions: %w[ActionController::RoutingError ForbiddenError],
+    deployment: {
+      revision: Rails.configuration.x.git.commit_version,
+    },
   )
 end
