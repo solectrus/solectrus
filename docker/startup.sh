2dde280d6e6da8fb8c38e0999f49f75d41f42068
Georg Ledermann <georg@ledermann.dev>
2023-12-25 13:04:42 +0100

    Docker: Wait for Redis on startup

--- docker/startup.sh
+++ docker/startup.sh
@@ -4,6 +4,15 @@ echo "Starting SOLECTRUS..."
 echo "Version: $COMMIT_VERSION - $COMMIT_TIME - $COMMIT_BRANCH"
 echo "----------------"
 
+# Wait for Redis
+redis_host=$(echo "$REDIS_URL" | awk -F[/:] '{print $4}')
+redis_port=$(echo "$REDIS_URL" | awk -F[/:] '{print $5}')
+until nc -z -v -w30 "$redis_host" "$redis_port"; do
+  echo "Waiting for Redis on $redis_host:$redis_port ..."
+  sleep 1
+done
+echo "Redis is up and running!"
+
 # Wait for InfluxDB
 until nc -z -v -w30 "$INFLUX_HOST" ${INFLUX_PORT:-8086}; do
   echo "Waiting for InfluxDB on $INFLUX_HOST:${INFLUX_PORT:-8086} ..."
