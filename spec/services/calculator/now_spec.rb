solectrus/solectrus

commit 9cf49fa6b86790bfd7aa9b79eb41ac3e145d6e15

Refactoring

app/services/calculator/base.rb
@@ -1,47 +1,29 @@
 class Calculator::Base
-  def build_context(hash_or_array)
-    case hash_or_array
-    when Array
-      array = hash_or_array
-      hash = array.first
-    when Hash
-      array = [hash_or_array]
-      hash = hash_or_array
-    else
-      # :nocov:
-      raise ArgumentError, 'Argument must be Hash or Array'
-      # :nocov:
+  def build_method(key, data = nil, &)
+    if data.nil? ^ block_given?
+      raise ArgumentError, 'Either data or block must be given, not both'
     end
 
-    # TODO: Extract to new base class and define static methods
-    hash.each_key do |key|
-      case key
-      when :time, :current_state, :current_state_ok, :power_ratio
-        value = array.pluck(key).last
+    define_singleton_method(key) { data ? data[key] : yield }
+  end
 
-        define_singleton_method(key) { value }
-      when :feed_in_tariff, :electricity_price
-        values = array.pluck(key)
+  def build_method_from_array(key, data)
+    values = data.pluck(key).map(&:to_f)
 
-        define_singleton_method(key) { values }
-      else
-        values = array.map { |v| v[key].to_f }
-
-        define_singleton_method(key) { values.sum }
-        define_singleton_method("#{key}_array") { values }
-      end
-    end
-
-    define_singleton_method(:sections) { array }
+    build_method("#{key}_array") { values }
+    build_method(key) { values.sum }
   end
 
   # Inverter
 
   def producing?
+    return unless inverter_power
+
     inverter_power >= 50
   end
 
   def inverter_power_percent
+    return unless inverter_power && total_plus
     return 0 if total_plus.zero?
 
     (inverter_power * 100.0 / total_plus).round(1)
@@ -50,22 +32,27 @@ class Calculator::Base
   # Grid
 
   def feeding?
+    return unless grid_power_plus && grid_power_minus
     return false if [grid_power_plus, grid_power_minus].compact.max < 50
 
     grid_power_minus > grid_power_plus
   end
 
   def grid_power
+    return unless grid_power_plus && grid_power_minus
+
     feeding? ? grid_power_minus : -grid_power_plus
   end
 
   def grid_power_plus_percent
+    return unless grid_power_plus && total_plus
     return 0 if total_plus.zero?
 
     (grid_power_plus * 100.0 / total_plus).round(1)
   end
 
   def grid_power_minus_percent
+    return unless grid_power_minus && total_minus
     return 0 if total_minus.zero?
 
     (grid_power_minus * 100.0 / total_minus).round(1)
@@ -74,6 +61,8 @@ class Calculator::Base
   # House
 
   def consumption
+    return unless house_power && wallbox_charge_power
+
     house_power + wallbox_charge_power
   end
 
@@ -84,16 +73,21 @@ class Calculator::Base
   end
 
   def consumption_alt
+    return unless inverter_power && grid_power_minus
+
     inverter_power - grid_power_minus
   end
 
   def consumption_quote
+    return unless consumption_alt && inverter_power
     return if inverter_power < 50
 
     [consumption_alt * 100.0 / inverter_power, 0.0].max.round(1)
   end
 
   def grid_quote
+    return unless consumption && grid_power_plus
+
     if consumption.zero?
       # Producing without any consumption
       #  => Maybe there is a balkony power plant
@@ -114,6 +108,7 @@ class Calculator::Base
   end
 
   def house_power_percent
+    return unless house_power && total_minus
     return 0 if total_minus.zero?
 
     (house_power * 100.0 / total_minus).round(1)
@@ -122,6 +117,7 @@ class Calculator::Base
   # Wallbox
 
   def wallbox_charge_power_percent
+    return unless wallbox_charge_power && total_minus
     return 0 if total_minus.zero?
 
     (wallbox_charge_power * 100.0 / total_minus).round(1)
@@ -130,24 +126,32 @@ class Calculator::Base
   # Battery
 
   def bat_empty?
+    return unless bat_fuel_charge
+
     bat_fuel_charge < 1
   end
 
   def bat_charging?
+    return unless bat_power_plus && bat_power_minus
+
     bat_power_plus > bat_power_minus
   end
 
   def bat_power
+    return unless bat_power_plus && bat_power_minus
+
     bat_charging? ? bat_power_plus : -bat_power_minus
   end
 
   def bat_power_minus_percent
+    return unless bat_power_minus && total_plus
     return 0 if total_plus.zero?
 
     (bat_power_minus * 100.0 / total_plus).round(1)
   end
 
   def bat_power_plus_percent
+    return unless bat_power_plus && total_minus
     return 0 if total_minus.zero?
 
     (bat_power_plus * 100.0 / total_minus).round(1)
@@ -156,14 +160,21 @@ class Calculator::Base
   # Total
 
   def total_plus
+    return unless grid_power_plus && bat_power_minus && inverter_power
+
     grid_power_plus + bat_power_minus + inverter_power
   end
 
   def total_minus
+    unless grid_power_minus && bat_power_plus && house_power &&
+             wallbox_charge_power
+      return
+    end
+
     grid_power_minus + bat_power_plus + house_power + wallbox_charge_power
   end
 
   def total
-    [total_minus, total_plus].max
+    [total_minus, total_plus].compact.max
   end
 end

app/services/calculator/now.rb
@@ -21,48 +21,26 @@ class Calculator::Now < Calculator::Base
                   ).call(Timeframe.now)
   end
 
-  def inverter_to_house
-    [
-      inverter_power - inverter_to_battery - inverter_to_wallbox,
-      house_power,
-    ].min
-  end
+  def build_context(data)
+    build_method(:time, data)
+    build_method(:current_state, data)
+    build_method(:current_state_ok, data)
 
-  def inverter_to_battery
-    bat_charging? && inverter_power >= bat_power_plus ? bat_power_plus : 0
-  end
-
-  def inverter_to_wallbox
-    producing? && wallbox_charge_power.positive? ? inverter_power : 0
-  end
-
-  def grid_to_house
-    [house_power - inverter_to_house - battery_to_house, 0].max
-  end
-
-  def grid_to_wallbox
-    wallbox_charge_power&.positive? ? grid_power_plus - grid_to_house : 0
-  end
-
-  def battery_to_house
-    bat_empty? ? 0 : bat_power_minus
-  end
-
-  def grid_to_battery
-    if bat_charging? && grid_power_plus > (house_power + wallbox_charge_power)
-      bat_power_plus
-    else
-      0
-    end
+    build_method(:inverter_power, data)
+    build_method(:house_power, data)
+    build_method(:wallbox_charge_power, data)
+    build_method(:grid_power_plus, data)
+    build_method(:grid_power_minus, data)
+    build_method(:bat_power_minus, data)
+    build_method(:bat_power_plus, data)
+    build_method(:bat_fuel_charge, data)
+    build_method(:case_temp, data)
+    build_method(:power_ratio, data)
   end
 
   def power_ratio_limited?
-    return false if power_ratio.nil?
+    return unless power_ratio
 
     power_ratio < 100
-  end
-
-  def house_to_grid
-    feeding? ? grid_power_minus : 0
   end
 end

app/services/calculator/range.rb
@@ -2,19 +2,18 @@ class Calculator::Range < Calculator::Base
   def initialize(timeframe)
     super()
 
-    sums =
-      PowerSum.new(
-        measurements: [
-          Rails.configuration.x.influx.measurement_pv,
-          Rails.configuration.x.influx.measurement_forecast,
-        ],
-        fields: fields(timeframe),
-      ).call(timeframe)
+    @timeframe = timeframe
 
-    build_context sums
+    build_context PowerSum.new(
+                    measurements: [
+                      Rails.configuration.x.influx.measurement_pv,
+                      Rails.configuration.x.influx.measurement_forecast,
+                    ],
+                    fields:,
+                  ).call(timeframe)
   end
 
-  def fields(timeframe)
+  def fields
     result = %i[
       inverter_power
       house_power
@@ -28,9 +27,26 @@ class Calculator::Range < Calculator::Base
     ]
 
     # Include forecast for days only
-    result << :watt if timeframe.day?
+    result << :watt if @timeframe.day?
 
     result
+  end
+
+  def build_context(data)
+    build_method(:sections) { data }
+    build_method(:time) { data.pluck(:time).last }
+
+    build_method_from_array(:feed_in_tariff, data)
+    build_method_from_array(:electricity_price, data)
+
+    build_method_from_array(:inverter_power, data)
+    build_method_from_array(:house_power, data)
+    build_method_from_array(:wallbox_charge_power, data)
+    build_method_from_array(:grid_power_plus, data)
+    build_method_from_array(:grid_power_minus, data)
+    build_method_from_array(:bat_power_minus, data)
+    build_method_from_array(:bat_power_plus, data)
+    build_method_from_array(:watt, data) if @timeframe.day?
   end
 
   def forecast_deviation
@@ -44,7 +60,7 @@ class Calculator::Range < Calculator::Base
     return unless grid_power_plus
 
     -section_sum do |index|
-      grid_power_plus_array[index] * electricity_price[index]
+      grid_power_plus_array[index] * electricity_price_array[index]
     end
   end
 
@@ -52,7 +68,7 @@ class Calculator::Range < Calculator::Base
     return unless grid_power_minus
 
     section_sum do |index|
-      grid_power_minus_array[index] * feed_in_tariff[index]
+      grid_power_minus_array[index] * feed_in_tariff_array[index]
     end
   end
 
@@ -65,7 +81,9 @@ class Calculator::Range < Calculator::Base
   def traditional_price
     return unless consumption
 
-    -section_sum { |index| consumption_array[index] * electricity_price[index] }
+    -section_sum do |index|
+      consumption_array[index] * electricity_price_array[index]
+    end
   end
 
   def savings
@@ -79,8 +97,8 @@ class Calculator::Range < Calculator::Base
 
     [
       section_sum do |index|
-        (bat_power_minus_array[index] * electricity_price[index]) -
-          (bat_power_plus_array[index] * feed_in_tariff[index])
+        (bat_power_minus_array[index] * electricity_price_array[index]) -
+          (bat_power_plus_array[index] * feed_in_tariff_array[index])
       end,
       savings,
     ].min
@@ -99,7 +117,7 @@ class Calculator::Range < Calculator::Base
     @wallbox_costs ||=
       -section_sum do |index|
         [wallbox_charge_power_array[index], grid_power_plus_array[index]].min *
-          electricity_price[index]
+          electricity_price_array[index]
       end
   end
 
@@ -108,13 +126,14 @@ class Calculator::Range < Calculator::Base
 
     @house_costs ||=
       -section_sum do |index|
-        total_costs = (grid_power_plus_array[index] * electricity_price[index])
+        total_costs =
+          (grid_power_plus_array[index] * electricity_price_array[index])
 
         wallbox_costs =
           [
             wallbox_charge_power_array[index],
             grid_power_plus_array[index],
-          ].min * electricity_price[index]
+          ].min * electricity_price_array[index]
 
         total_costs - wallbox_costs
       end

spec/services/calculator/base_spec.rb
@@ -1,12 +1,65 @@
 describe Calculator::Base do
   let(:calculator) { described_class.new }
 
+  describe '#build_method' do
+    subject { calculator.public_send(method) }
+
+    let(:method) { :foo }
+
+    context 'when block is given' do
+      before { calculator.build_method(method) { 42 } }
+
+      it { is_expected.to eq(42) }
+    end
+
+    context 'when data is given' do
+      let(:data) { { method => 42 } }
+
+      before { calculator.build_method(method, data) }
+
+      it { is_expected.to eq(42) }
+    end
+
+    context 'when neither data nor block is given' do
+      it 'raises an ArgumentError' do
+        expect { calculator.build_method(:foo) }.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'when both data and block are given' do
+      it 'raises an ArgumentError' do
+        expect do
+          calculator.build_method(:foo, { foo: 42 }) { 42 }
+        end.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe '#build_method_from_array' do
+    let(:method) { :foo }
+
+    context 'when data is given' do
+      let(:data) { [{ method => 42 }, { method => 43 }] }
+
+      before { calculator.build_method_from_array(method, data) }
+
+      it 'defines a method for sum' do
+        expect(calculator.public_send(method)).to eq(85)
+      end
+
+      it 'defines a method for array' do
+        expect(calculator.public_send("#{method}_array")).to eq([42, 43])
+      end
+    end
+  end
+
   describe '#grid_power' do
     subject { calculator.grid_power }
 
     context 'when grid_power_plus > grid_power_minus' do
       before do
-        calculator.build_context grid_power_plus: 3_900, grid_power_minus: 0
+        calculator.build_method(:grid_power_plus) { 3_900 }
+        calculator.build_method(:grid_power_minus) { 0 }
       end
 
       it { is_expected.to eq(-3_900) }
@@ -14,7 +67,8 @@ describe Calculator::Base do
 
     context 'when grid_power_plus < grid_power_minus' do
       before do
-        calculator.build_context grid_power_minus: 3_900, grid_power_plus: 0
+        calculator.build_method(:grid_power_plus) { 0 }
+        calculator.build_method(:grid_power_minus) { 3_900 }
       end
 
       it { is_expected.to eq(3_900) }
@@ -26,7 +80,8 @@ describe Calculator::Base do
 
     context 'when bat_power_plus > bat_power_minus' do
       before do
-        calculator.build_context bat_power_plus: 1_500, bat_power_minus: 0
+        calculator.build_method(:bat_power_plus) { 1_500 }
+        calculator.build_method(:bat_power_minus) { 0 }
       end
 
       it { is_expected.to eq(1_500) }
@@ -34,10 +89,25 @@ describe Calculator::Base do
 
     context 'when bat_power_plus < bat_power_minus' do
       before do
-        calculator.build_context bat_power_minus: 1_500, bat_power_plus: 0
+        calculator.build_method(:bat_power_plus) { 0 }
+        calculator.build_method(:bat_power_minus) { 1_500 }
       end
 
       it { is_expected.to eq(-1_500) }
+    end
+  end
+
+  describe '#total_plus' do
+    subject { calculator.total_plus }
+
+    context 'when all is nil' do
+      before do
+        calculator.build_method(:grid_power_plus, {})
+        calculator.build_method(:bat_power_minus, {})
+        calculator.build_method(:inverter_power, {})
+      end
+
+      it { is_expected.to be_nil }
     end
   end
 
@@ -46,9 +116,9 @@ describe Calculator::Base do
 
     context 'with real data' do
       before do
-        calculator.build_context house_power: 6_800,
-                                 wallbox_charge_power: 0,
-                                 grid_power_plus: 3_900
+        calculator.build_method(:house_power) { 6_800 }
+        calculator.build_method(:wallbox_charge_power) { 0 }
+        calculator.build_method(:grid_power_plus) { 3_900 }
       end
 
       it { is_expected.to eq(42.6) }
@@ -56,9 +126,9 @@ describe Calculator::Base do
 
     context 'with simple example' do
       before do
-        calculator.build_context house_power: 3_500,
-                                 wallbox_charge_power: 0,
-                                 grid_power_plus: 2_500
+        calculator.build_method(:house_power) { 3_500 }
+        calculator.build_method(:wallbox_charge_power) { 0 }
+        calculator.build_method(:grid_power_plus) { 2_500 }
       end
 
       it { is_expected.to eq(28.6) }
@@ -66,9 +136,9 @@ describe Calculator::Base do
 
     context 'with rounding issues' do
       before do
-        calculator.build_context house_power: 317,
-                                 wallbox_charge_power: 0,
-                                 grid_power_plus: 308
+        calculator.build_method(:house_power) { 317 }
+        calculator.build_method(:wallbox_charge_power) { 0 }
+        calculator.build_method(:grid_power_plus) { 308 }
       end
 
       it { is_expected.to eq(2.8) }
@@ -76,9 +146,9 @@ describe Calculator::Base do
 
     context 'with wallbox' do
       before do
-        calculator.build_context house_power: 500,
-                                 wallbox_charge_power: 9_000,
-                                 grid_power_plus: 5_000
+        calculator.build_method(:house_power) { 500 }
+        calculator.build_method(:wallbox_charge_power) { 9_000 }
+        calculator.build_method(:grid_power_plus) { 5_000 }
       end
 
       it { is_expected.to eq(47.4) }
@@ -86,10 +156,10 @@ describe Calculator::Base do
 
     context 'with zero values' do
       before do
-        calculator.build_context inverter_power: 0,
-                                 house_power: 0,
-                                 wallbox_charge_power: 0,
-                                 grid_power_plus: 0
+        calculator.build_method(:inverter_power) { 0 }
+        calculator.build_method(:house_power) { 0 }
+        calculator.build_method(:wallbox_charge_power) { 0 }
+        calculator.build_method(:grid_power_plus) { 0 }
       end
 
       it { is_expected.to be_nil }
@@ -97,11 +167,11 @@ describe Calculator::Base do
 
     context 'with zero grid_power_plus (maybe caused by balcony power plant)' do
       before do
-        calculator.build_context house_power: 0,
-                                 wallbox_charge_power: 0,
-                                 inverter_power: 8_600,
-                                 grid_power_plus: 0,
-                                 grid_power_minus: 8_700
+        calculator.build_method(:house_power) { 0 }
+        calculator.build_method(:wallbox_charge_power) { 0 }
+        calculator.build_method(:inverter_power) { 8_600 }
+        calculator.build_method(:grid_power_plus) { 0 }
+        calculator.build_method(:grid_power_minus) { 8_700 }
       end
 
       it { is_expected.to eq(100) }
@@ -111,13 +181,13 @@ describe Calculator::Base do
       subject { calculator.current_state_ok }
 
       context 'when false' do
-        before { calculator.build_context current_state_ok: false }
+        before { calculator.build_method(:current_state_ok) { false } }
 
         it { is_expected.to be(false) }
       end
 
       context 'when true' do
-        before { calculator.build_context current_state_ok: true }
+        before { calculator.build_method(:current_state_ok) { true } }
 
         it { is_expected.to be(true) }
       end

commit 824a40dcb67fd821efad34d814bb89e475125aae

Calculator: Fix handling misising values
This fixes a regression introduced by 9cf49fa6b86790bfd7aa9b79eb41ac3e145d6e15

Belongs to #2647

app/services/calculator/base.rb
@@ -1,14 +1,23 @@
 class Calculator::Base
-  def build_method(key, data = nil, &)
+  def build_method(key, data = nil, modifier = nil, &)
     if data.nil? ^ block_given?
       raise ArgumentError, 'Either data or block must be given, not both'
     end
 
-    define_singleton_method(key) { data ? data[key] : yield }
+    define_singleton_method(key) do
+      result = (data ? data[key] : yield)
+      result = result.public_send(modifier) if modifier
+      result
+    end
   end
 
-  def build_method_from_array(key, data)
-    values = data.pluck(key).map(&:to_f)
+  def build_method_from_array(key, data, modifier = nil)
+    values =
+      if modifier
+        data.pluck(key).map { |value| value.public_send(modifier) }
+      else
+        data.pluck(key)
+      end
 
     build_method("#{key}_array") { values }
     build_method(key) { values.sum }

app/services/calculator/now.rb
@@ -26,16 +26,16 @@ class Calculator::Now < Calculator::Base
     build_method(:current_state, data)
     build_method(:current_state_ok, data)
 
-    build_method(:inverter_power, data)
-    build_method(:house_power, data)
-    build_method(:wallbox_charge_power, data)
-    build_method(:grid_power_plus, data)
-    build_method(:grid_power_minus, data)
-    build_method(:bat_power_minus, data)
-    build_method(:bat_power_plus, data)
-    build_method(:bat_fuel_charge, data)
-    build_method(:case_temp, data)
-    build_method(:power_ratio, data)
+    build_method(:inverter_power, data, :to_f)
+    build_method(:house_power, data, :to_f)
+    build_method(:wallbox_charge_power, data, :to_f)
+    build_method(:grid_power_plus, data, :to_f)
+    build_method(:grid_power_minus, data, :to_f)
+    build_method(:bat_power_minus, data, :to_f)
+    build_method(:bat_power_plus, data, :to_f)
+    build_method(:bat_fuel_charge, data, :to_f)
+    build_method(:case_temp, data, :to_f)
+    build_method(:power_ratio, data, :to_f)
   end
 
   def power_ratio_limited?

app/services/calculator/range.rb
@@ -37,17 +37,17 @@ class Calculator::Range < Calculator::Base
     build_method(:sections) { data }
     build_method(:time) { data.pluck(:time).last }
 
-    build_method_from_array(:feed_in_tariff, data)
-    build_method_from_array(:electricity_price, data)
+    build_method_from_array(:feed_in_tariff, data, :to_f)
+    build_method_from_array(:electricity_price, data, :to_f)
 
-    build_method_from_array(:inverter_power, data)
-    build_method_from_array(:house_power, data)
-    build_method_from_array(:wallbox_charge_power, data)
-    build_method_from_array(:grid_power_plus, data)
-    build_method_from_array(:grid_power_minus, data)
-    build_method_from_array(:bat_power_minus, data)
-    build_method_from_array(:bat_power_plus, data)
-    build_method_from_array(:watt, data) if @timeframe.day?
+    build_method_from_array(:inverter_power, data, :to_f)
+    build_method_from_array(:house_power, data, :to_f)
+    build_method_from_array(:wallbox_charge_power, data, :to_f)
+    build_method_from_array(:grid_power_plus, data, :to_f)
+    build_method_from_array(:grid_power_minus, data, :to_f)
+    build_method_from_array(:bat_power_minus, data, :to_f)
+    build_method_from_array(:bat_power_plus, data, :to_f)
+    build_method_from_array(:watt, data, :to_f) if @timeframe.day?
   end
 
   def forecast_deviation

spec/services/calculator/base_spec.rb
@@ -20,6 +20,14 @@ describe Calculator::Base do
       it { is_expected.to eq(42) }
     end
 
+    context 'when modifier is given' do
+      let(:data) { { method => nil } }
+
+      before { calculator.build_method(method, data, :to_f) }
+
+      it { is_expected.to eq(0.0) }
+    end
+
     context 'when neither data nor block is given' do
       it 'raises an ArgumentError' do
         expect { calculator.build_method(:foo) }.to raise_error(ArgumentError)

spec/services/calculator/now_spec.rb
@@ -8,13 +8,21 @@ describe Calculator::Now do
       add_influx_point(
         name: Rails.configuration.x.influx.measurement_pv,
         fields: {
-          inverter_power: 0,
+          inverter_power: 10,
         },
       )
     end
 
     it 'returns time of measurement' do
       expect(calculator.time).to eq(Time.current)
+    end
+
+    it 'returns existing value as float' do
+      expect(calculator.inverter_power).to eq(10.0)
+    end
+
+    it 'returns missing value as 0' do
+      expect(calculator.wallbox_charge_power).to eq(0)
     end
   end
 end