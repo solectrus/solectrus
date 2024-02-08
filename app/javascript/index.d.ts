7eeff2cce0fd2cb00e2449b98246f5f5c82aa5fc
Georg Ledermann <georg@ledermann.dev>
2024-02-08 04:18:20 +0100

    :arrow_up: Upgrade Turbo to v8

--- app/javascript/index.d.ts
+++ app/javascript/index.d.ts
@@ -3,3 +3,27 @@ declare module 'el-transition' {
   function leave(el: HTMLElement, transitionName?: string): Promise<void>;
   function toggle(el: HTMLElement, transitionName?: string): Promise<void>;
 }
+
+// Dummy declaration for Turbo 8
+declare module '@hotwired/turbo' {
+  export class FrameElement extends HTMLElement {
+    reload(): Promise<void>;
+  }
+
+  export type TurboFrameMissingEvent = CustomEvent<{
+    response: Response;
+  }>;
+
+  interface StreamActionContext {
+    hasAttribute(attributeName: string): boolean;
+    templateContent: DocumentFragment;
+    templateElement: HTMLTemplateElement;
+    targetElements: Element[];
+  }
+
+  export namespace StreamActions {
+    export let morph: (this: StreamActionContext) => void;
+  }
+
+  export function visit(url: string, options?): void;
+}
