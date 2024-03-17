3af3bdb0caa908bdebf006b9ceabb72cd7d7edf3
Georg Ledermann <georg@ledermann.dev>
2024-03-17 12:08:30 +0100

    Footer: Show icon when registration status is not complete

---
+++ app/components/registration_status/component.rb
@@ -0,0 +1,16 @@
+class RegistrationStatus::Component < ViewComponent::Base
+  def initialize(registration_status:, admin:)
+    super
+    @registration_status = registration_status.presence
+    @admin = admin
+  end
+
+  attr_reader :registration_status, :admin
+
+  delegate :pending?,
+           :unregistered?,
+           :unknown?,
+           :complete?,
+           to: :registration_status,
+           allow_nil: true
+end
