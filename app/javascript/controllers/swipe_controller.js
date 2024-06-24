95621c6fc3b168d1b24ed15d3d11ee4ca7b6d23f
Georg Ledermann <georg@ledermann.dev>
2024-06-24 07:28:21 +0200

    Hammer.js is not maintained anymore
    https://github.com/hammerjs/hammer.js/issues/1278

--- app/javascript/controllers/swipe_controller.js
+++ app/javascript/controllers/swipe_controller.js
@@ -1,27 +1,61 @@
 import { Controller } from '@hotwired/stimulus';
-import Hammer from 'hammerjs';
 
 export default class extends Controller {
   connect() {
-    const hammer = new Hammer(this.element);
+    this.touchStartX = 0;
+    this.touchEndX = 0;
 
-    hammer.on('swiperight', () => {
-      const prevLink = document.querySelector('[rel="prev"]');
-      if (prevLink) {
-        prevLink.click();
-      }
-    });
+    this.element.addEventListener(
+      'touchstart',
+      this.handleTouchStart.bind(this),
+      false,
+    );
+    this.element.addEventListener(
+      'touchend',
+      this.handleTouchEnd.bind(this),
+      false,
+    );
+  }
 
-    hammer.on('swipeleft', () => {
-      const nextLink = document.querySelector('[rel="next"]');
-      if (nextLink) {
-        nextLink.click();
-      }
-    });
+  handleTouchStart(event) {
+    this.touchStartX = event.changedTouches[0].screenX;
+  }
+
+  handleTouchEnd(event) {
+    this.touchEndX = event.changedTouches[0].screenX;
+    this.handleGesture();
+  }
+
+  handleGesture() {
+    if (this.touchEndX < this.touchStartX) {
+      this.swipeLeft();
+    } else if (this.touchEndX > this.touchStartX) {
+      this.swipeRight();
+    }
+  }
+
+  swipeLeft() {
+    const nextLink = document.querySelector('[rel="next"]');
+    if (nextLink) {
+      nextLink.click();
+    }
+  }
+
+  swipeRight() {
+    const prevLink = document.querySelector('[rel="prev"]');
+    if (prevLink) {
+      prevLink.click();
+    }
   }
 
   disconnect() {
-    const hammer = Hammer(this.element);
-    hammer.off('swiperight swipeleft');
+    this.element.removeEventListener(
+      'touchstart',
+      this.handleTouchStart.bind(this),
+    );
+    this.element.removeEventListener(
+      'touchend',
+      this.handleTouchEnd.bind(this),
+    );
   }
 }
