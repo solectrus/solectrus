ed3ae8891a06d8f3ae5367c0f77da690b42da81e
Georg Ledermann <georg@ledermann.dev>
2024-06-30 10:09:26 +0200

    Add test for AutarkyChart when there is no wallbox sensor

--- spec/services/autarky_chart_spec.rb
+++ spec/services/autarky_chart_spec.rb
@@ -3,65 +3,133 @@ describe AutarkyChart do
 
   let(:beginning) { 1.year.ago.beginning_of_year }
 
-  before do
-    influx_batch do
-      12.times do |index|
+  around { |example| freeze_time(&example) }
+
+  context 'with wallbox_power sensor' do
+    before do
+      influx_batch do
+        12.times do |index|
+          add_influx_point name: measurement_inverter_power,
+                           fields: {
+                             field_house_power => (index + 1) * 100,
+                             field_grid_import_power => (index + 1) * 300,
+                             field_wallbox_power => (index + 1) * 500,
+                           },
+                           time: (beginning + index.month).end_of_month
+          add_influx_point name: measurement_inverter_power,
+                           fields: {
+                             field_house_power => (index + 1) * 100,
+                             field_grid_import_power => (index + 1) * 300,
+                             field_wallbox_power => (index + 1) * 500,
+                           },
+                           time: (beginning + index.month).beginning_of_month
+        end
+
         add_influx_point name: measurement_inverter_power,
                          fields: {
-                           field_house_power => (index + 1) * 100,
-                           field_grid_import_power => (index + 1) * 300,
-                           field_wallbox_power => (index + 1) * 500,
-                         },
-                         time: (beginning + index.month).end_of_month
-        add_influx_point name: measurement_inverter_power,
-                         fields: {
-                           field_house_power => (index + 1) * 100,
-                           field_grid_import_power => (index + 1) * 300,
-                           field_wallbox_power => (index + 1) * 500,
-                         },
-                         time: (beginning + index.month).beginning_of_month
+                           field_house_power => 6_000,
+                           field_grid_import_power => 3000,
+                           # NOTE: There is no `wallbox_power` in this data point.
+                           # This happens when the `csv-importer` was used to import CSV data from SENEC,
+                           # which do not contain the `wallbox_power`.
+                           # The missing value tests the `if` statement in the query.
+                         }
+      end
+    end
+
+    describe '#call' do
+      subject(:result) { chart.call(timeframe) }
+
+      context 'when timeframe is "now"' do
+        let(:timeframe) { Timeframe.now }
+
+        it { is_expected.to have(1.hour / 30.seconds).items }
+
+        it 'contains last data point' do
+          last = result.last
+
+          expect(last[1]).to eq(50.0)
+          expect(last.first).to be_within(30.seconds).of(Time.current)
+        end
       end
 
-      add_influx_point name: measurement_inverter_power,
-                       fields: {
-                         field_house_power => 6_000,
-                         field_grid_import_power => 3000,
-                         # NOTE: There is no `wallbox_power` in this data point.
-                         # This happens when the `csv-importer` was used to import CSV data from SENEC,
-                         # which do not contain the `wallbox_power`.
-                         # The missing value tests the `if` statement in the query.
-                       }
+      context 'when timeframe is a year' do
+        let(:timeframe) { Timeframe.new(beginning.year.to_s) }
+
+        it { is_expected.to have(12).items }
+
+        it 'contains last and first data point' do
+          expect(result.first).to eq([beginning, 50.0])
+          expect(result.last).to eq(
+            [beginning.end_of_year.beginning_of_month, 50.0],
+          )
+        end
+      end
     end
   end
 
-  around { |example| freeze_time(&example) }
+  context 'without wallbox_power sensor' do
+    before do
+      allow(SensorConfig.x).to receive(:field) do |arg|
+        if arg == :wallbox_power
+          nil
+        else
+          original_method = SensorConfig.x.method(:field).super_method
+          original_method.call(arg)
+        end
+      end
 
-  describe '#call' do
-    subject(:result) { chart.call(timeframe) }
+      influx_batch do
+        12.times do |index|
+          add_influx_point name: measurement_inverter_power,
+                           fields: {
+                             field_house_power => (index + 1) * 100,
+                             field_grid_import_power => (index + 1) * 50,
+                           },
+                           time: (beginning + index.month).end_of_month
+          add_influx_point name: measurement_inverter_power,
+                           fields: {
+                             field_house_power => (index + 1) * 100,
+                             field_grid_import_power => (index + 1) * 50,
+                           },
+                           time: (beginning + index.month).beginning_of_month
+        end
 
-    context 'when timeframe is "now"' do
-      let(:timeframe) { Timeframe.now }
-
-      it { is_expected.to have(1.hour / 30.seconds).items }
-
-      it 'contains last data point' do
-        last = result.last
-
-        expect(last[1]).to eq(50.0)
-        expect(last.first).to be_within(30.seconds).of(Time.current)
+        add_influx_point name: measurement_inverter_power,
+                         fields: {
+                           field_house_power => 6_000,
+                           field_grid_import_power => 3000,
+                         }
       end
     end
 
-    context 'when timeframe is a year' do
-      let(:timeframe) { Timeframe.new(beginning.year.to_s) }
+    describe '#call' do
+      subject(:result) { chart.call(timeframe) }
 
-      it { is_expected.to have(12).items }
+      context 'when timeframe is "now"' do
+        let(:timeframe) { Timeframe.now }
 
-      it 'contains last and first data point' do
-        expect(result.first).to eq([beginning, 50.0])
-        expect(result.last).to eq(
-          [beginning.end_of_year.beginning_of_month, 50.0],
-        )
+        it { is_expected.to have(1.hour / 30.seconds).items }
+
+        it 'contains last data point' do
+          last = result.last
+
+          expect(last[1]).to eq(50.0)
+          expect(last.first).to be_within(30.seconds).of(Time.current)
+        end
+      end
+
+      context 'when timeframe is a year' do
+        let(:timeframe) { Timeframe.new(beginning.year.to_s) }
+
+        it { is_expected.to have(12).items }
+
+        it 'contains last and first data point' do
+          expect(result.first).to eq([beginning, 50.0])
+          expect(result.last).to eq(
+            [beginning.end_of_year.beginning_of_month, 50.0],
+          )
+        end
       end
     end
   end
