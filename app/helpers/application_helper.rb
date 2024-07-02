0ef6dcef9f119eb22335d0ad53f8fd591f6498a7
Georg Ledermann <georg@ledermann.dev>
2024-07-02 09:23:24 +0200

    Don't render banner in case of an error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
   def banner?
-    (
-      UpdateCheck.instance.registration_status.in?(%w[unregistered pending]) ||
-        UpdateCheck.instance.prompt?
-    ) && !UpdateCheck.instance.skipped_prompt?
+    return false if controller.is_a?(ErrorsController)
+    return false if UpdateCheck.instance.skipped_prompt?
+
+    UpdateCheck.instance.prompt? ||
+      UpdateCheck.instance.registration_status.in?(%w[unregistered pending])
   end
 
   def extra_stimulus_controllers(*controller_names)
