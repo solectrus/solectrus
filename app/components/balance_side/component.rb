3b0efe190057e4a17155267df47de606f751affe
Georg Ledermann <georg@ledermann.dev>
2024-07-03 14:43:31 +0200

    Don't render balance segment when zero

--- app/components/balance_side/component.rb
+++ app/components/balance_side/component.rb
@@ -1,7 +1,7 @@
 class BalanceSide::Component < ViewComponent::Base
   renders_many :segments,
                lambda { |sensor, peak = nil|
-                 if SensorConfig.x.exists?(sensor)
+                 if render_segment?(sensor)
                    BalanceSegment::Component.new sensor:, peak:, parent: self
                  end
                }
@@ -18,5 +18,19 @@ class BalanceSide::Component < ViewComponent::Base
 
   def title
     I18n.t "balance_sheet.#{side}"
+  end
+
+  private
+
+  def render_segment?(sensor)
+    # We never want a segment for a non-existing sensor
+    return false unless SensorConfig.x.exists?(sensor)
+
+    # On the "now" page the height is animated, so
+    # we need to render even if the value is curently zero
+    return true if timeframe.now?
+
+    # Otherwise, we don't need a segment when value is zero
+    calculator.public_send(sensor).nonzero?
   end
 end
