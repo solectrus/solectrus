b8b59887509797c2cee21a9f8a02d2c812dcc3d8
Georg Ledermann <georg@ledermann.dev>
2024-07-15 07:38:56 +0200

    Top10: Add heatpump test

--- spec/cypress/integration/top10.spec.js
+++ spec/cypress/integration/top10.spec.js
@@ -7,16 +7,19 @@ describe('Top 10', () => {
     'battery_discharging_power',
     'battery_charging_power',
     'wallbox_power',
+    'heatpump_power',
   ].forEach((sensor) => {
     it(`${sensor} is clickable`, () => {
+      // Days
       cy.visit(`/top10/day/${sensor}/sum/desc`);
-
       cy.get('#chart-day').should('exist');
 
+      // Weeks
       cy.contains('Woche').click();
       cy.location('pathname').should('equal', `/top10/week/${sensor}/sum/desc`);
       cy.get('#chart-week').should('exist');
 
+      // Months
       cy.contains('Monat').click();
       cy.location('pathname').should(
         'equal',
@@ -24,15 +27,18 @@ describe('Top 10', () => {
       );
       cy.get('#chart-month').should('exist');
 
+      // Yeary
       cy.contains('Jahr').click();
       cy.location('pathname').should('equal', `/top10/year/${sensor}/sum/desc`);
       cy.get('#chart-year').should('exist');
 
+      // Change sorting to asc
       cy.get('[aria-label="Sortierung wechseln"]').click();
       cy.location('pathname').should('equal', `/top10/year/${sensor}/sum/asc`);
       cy.contains('Nicht genügend Daten vorhanden.').should('be.visible');
       cy.get('#chart-year').should('not.exist');
 
+      // Change sorting to desc
       cy.get('[aria-label="Sortierung wechseln"]').click();
       cy.location('pathname').should('equal', `/top10/year/${sensor}/sum/desc`);
       cy.get('#chart-year').should('exist');
