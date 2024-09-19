b0e1e5dcb41e738ee216e11e00f74f41fbaba1d5
Georg Ledermann <georg@ledermann.dev>
2024-09-19 11:59:49 +0200

    Use blank layout for sponsoring page

--- app/controllers/sponsorings_controller.rb
+++ app/controllers/sponsorings_controller.rb
@@ -1,6 +1,8 @@
 class SponsoringsController < ApplicationController
   skip_before_action :check_for_sponsoring
 
+  layout 'blank'
+
   def show
     if UpdateCheck.eligible_for_free? || UpdateCheck.sponsoring?
       redirect_to root_path
