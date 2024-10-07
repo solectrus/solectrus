0a944e7d4fe668be3275240058590d7ef2a3daa8
Georg Ledermann <georg@ledermann.dev>
2024-10-07 11:15:15 +0200

    ESLint: Avoid FlatCompat

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,16 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-import { FlatCompat } from '@eslint/eslintrc';
-const compat = new FlatCompat();
+import pluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import pluginTailwindcss from 'eslint-plugin-tailwindcss';
+import pluginCypress from 'eslint-plugin-cypress/flat';
 
 export default [
-  ...compat.env({
-    browser: true,
-    node: true,
-  }),
-
-  ...compat.config({
-    extends: [
-      'prettier',
-      'plugin:prettier/recommended',
-      'plugin:tailwindcss/recommended',
-      'plugin:cypress/recommended',
-    ],
-    rules: {
-      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-      'tailwindcss/no-custom-classname': 'off',
-    },
-  }),
-
-  ...[eslint.configs.recommended, ...tseslint.configs.recommended].map(
-    (conf) => ({
-      ...conf,
-      files: ['**/*.ts'],
-    }),
-  ),
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginPrettierRecommended,
+  ...pluginTailwindcss.configs['flat/recommended'],
+  pluginCypress.configs.recommended,
 
   {
     ignores: [
