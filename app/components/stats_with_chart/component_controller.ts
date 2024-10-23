solectrus/solectrus

commit eb34d1c790b71e4901eee8e5cddde65026b09131

Fix removing event listeners
https://www.betterstimulus.com/events/events

app/components/stats_with_chart/component_controller.ts
@@ -44,15 +44,18 @@ export default class extends Controller {
 
   private interval: ReturnType<typeof setInterval> | undefined;
   private selectedSensor?: string;
+  private boundHandleVisibilityChange?: () => void;
 
   connect() {
+    this.boundHandleVisibilityChange = this.handleVisibilityChange.bind(this);
+
     if (this.intervalValue)
       document.addEventListener(
         'visibilitychange',
-        this.handleVisibilityChange.bind(this),
+        this.boundHandleVisibilityChange,
       );
 
-    document.addEventListener('dblclick', this.handleDblClick.bind(this));
+    document.addEventListener('dblclick', this.boundHandleVisibilityChange);
 
     this.startLoop();
   }
@@ -60,13 +63,18 @@ export default class extends Controller {
   disconnect() {
     this.stopLoop();
 
-    document.removeEventListener('dblclick', this.handleDblClick.bind(this));
-
-    if (this.intervalValue)
+    if (this.boundHandleVisibilityChange) {
       document.removeEventListener(
-        'visibilitychange',
-        this.handleVisibilityChange.bind(this),
+        'dblclick',
+        this.boundHandleVisibilityChange,
       );
+
+      if (this.intervalValue)
+        document.removeEventListener(
+          'visibilitychange',
+          this.boundHandleVisibilityChange,
+        );
+    }
   }
 
   startLoop(event?: ActionEvent) {

app/components/chart_loader/component_controller.ts
@@ -61,19 +61,19 @@ export default class extends Controller<HTMLCanvasElement> {
   declare unitValue: string;
   declare readonly hasUnitValue: boolean;
 
+  private boundHandleResize?: () => void;
   private chart?: Chart;
 
   connect() {
     this.process();
 
-    window.addEventListener(
-      'resize',
-      debounce(100, this.handleResize.bind(this)),
-    );
+    this.boundHandleResize = debounce(100, this.handleResize.bind(this));
+    window.addEventListener('resize', this.boundHandleResize);
   }
 
   disconnect() {
-    window.removeEventListener('resize', this.handleResize.bind(this));
+    if (this.boundHandleResize)
+      window.removeEventListener('resize', this.boundHandleResize);
 
     if (this.chart) this.chart.destroy();
   }

app/components/dropdown/component_controller.ts
@@ -14,12 +14,16 @@ export default class extends Controller {
   declare readonly buttonTarget: HTMLElement;
   declare readonly buttonTargets: HTMLElement[];
 
+  private boundHandleClickOutside?: (event: Event) => void;
+
   connect() {
-    document.addEventListener('click', this.handleClickOutside.bind(this));
+    this.boundHandleClickOutside = this.handleClickOutside.bind(this);
+    document.addEventListener('click', this.boundHandleClickOutside);
   }
 
   disconnect() {
-    document.removeEventListener('click', this.handleClickOutside.bind(this));
+    if (this.boundHandleClickOutside)
+      document.removeEventListener('click', this.boundHandleClickOutside);
   }
 
   toggle() {

app/components/essentials_tile/component_controller.ts
@@ -10,6 +10,8 @@ export default class extends Controller {
 
   declare intervalValue: number;
 
+  private boundHandleVisibilityChange?: () => void;
+
   connect() {
     this.startLoop();
     this.addListeners();
@@ -21,17 +23,19 @@ export default class extends Controller {
   }
 
   addListeners(): void {
+    this.boundHandleVisibilityChange = this.handleVisibilityChange.bind(this);
     document.addEventListener(
       'visibilitychange',
-      this.handleVisibilityChange.bind(this),
+      this.boundHandleVisibilityChange,
     );
   }
 
   removeListeners(): void {
-    document.removeEventListener(
-      'visibilitychange',
-      this.handleVisibilityChange.bind(this),
-    );
+    if (this.boundHandleVisibilityChange)
+      document.removeEventListener(
+        'visibilitychange',
+        this.boundHandleVisibilityChange,
+      );
   }
 
   startLoop() {

app/javascript/controllers/fullscreen_controller.ts
@@ -6,21 +6,27 @@ export default class extends Controller {
   declare readonly btnOnTargets: HTMLElement[];
   declare readonly btnOffTargets: HTMLElement[];
 
+  private boundFullscreenChangeListener?: () => void;
+
   connect() {
     this.updateButtons();
 
-    if (this.isFullscreenSupported())
+    if (this.isFullscreenSupported()) {
+      this.boundFullscreenChangeListener =
+        this.fullscreenChangeListener.bind(this);
+
       document.addEventListener(
         'fullscreenchange',
-        this.fullscreenChangeListener.bind(this),
+        this.boundFullscreenChangeListener,
       );
+    }
   }
 
   disconnect() {
-    if (this.isFullscreenSupported())
+    if (this.boundFullscreenChangeListener)
       document.removeEventListener(
         'fullscreenchange',
-        this.fullscreenChangeListener.bind(this),
+        this.boundFullscreenChangeListener,
       );
   }
 

app/javascript/controllers/swipe_controller.ts
@@ -10,22 +10,24 @@ export default class extends Controller<HTMLElement> {
   private touchStartTime: number = 0;
   private touchEndTime: number = 0;
 
+  private boundHandleTouchStart?: (event: TouchEvent) => void;
+  private boundHandleTouchEnd?: (event: TouchEvent) => void;
+
   connect() {
     if (!isTouchEnabled()) return;
 
     // Prevent rubber-band scrolling when swiping
     document.body.classList.add('overflow-hidden');
 
+    this.boundHandleTouchStart = this.handleTouchStart.bind(this);
     this.element.addEventListener(
       'touchstart',
-      this.handleTouchStart.bind(this),
+      this.boundHandleTouchStart,
       false,
     );
-    this.element.addEventListener(
-      'touchend',
-      this.handleTouchEnd.bind(this),
-      false,
-    );
+
+    this.boundHandleTouchEnd = this.handleTouchEnd.bind(this);
+    this.element.addEventListener('touchend', this.boundHandleTouchEnd, false);
   }
 
   handleTouchStart(event: TouchEvent) {
@@ -85,13 +87,12 @@ export default class extends Controller<HTMLElement> {
   disconnect() {
     if (!isTouchEnabled()) return;
 
-    this.element.removeEventListener(
-      'touchstart',
-      this.handleTouchStart.bind(this),
-    );
-    this.element.removeEventListener(
-      'touchend',
-      this.handleTouchEnd.bind(this),
-    );
+    if (this.boundHandleTouchStart)
+      this.element.removeEventListener(
+        'touchstart',
+        this.boundHandleTouchStart,
+      );
+    if (this.boundHandleTouchEnd)
+      this.element.removeEventListener('touchend', this.boundHandleTouchEnd);
   }
 }

commit 2e4e3ade51dd4975ebd63ecfd278770592d88dfb

StatsWithChart: Fix event handling
This fixes a bug caused by eb34d1c790b71e4901eee8e5cddde65026b09131

app/components/stats_with_chart/component_controller.ts
@@ -45,17 +45,19 @@ export default class extends Controller {
   private interval: ReturnType<typeof setInterval> | undefined;
   private selectedSensor?: string;
   private boundHandleVisibilityChange?: () => void;
+  private boundHandleDblClick?: (event: MouseEvent) => void;
 
   connect() {
-    this.boundHandleVisibilityChange = this.handleVisibilityChange.bind(this);
-
-    if (this.intervalValue)
+    if (this.intervalValue) {
+      this.boundHandleVisibilityChange = this.handleVisibilityChange.bind(this);
       document.addEventListener(
         'visibilitychange',
         this.boundHandleVisibilityChange,
       );
+    }
 
-    document.addEventListener('dblclick', this.boundHandleVisibilityChange);
+    this.boundHandleDblClick = this.handleDblClick.bind(this);
+    document.addEventListener('dblclick', this.boundHandleDblClick);
 
     this.startLoop();
   }
@@ -63,18 +65,15 @@ export default class extends Controller {
   disconnect() {
     this.stopLoop();
 
-    if (this.boundHandleVisibilityChange) {
+    if (this.boundHandleDblClick) {
+      document.removeEventListener('dblclick', this.boundHandleDblClick);
+    }
+
+    if (this.boundHandleVisibilityChange)
       document.removeEventListener(
-        'dblclick',
+        'visibilitychange',
         this.boundHandleVisibilityChange,
       );
-
-      if (this.intervalValue)
-        document.removeEventListener(
-          'visibilitychange',
-          this.boundHandleVisibilityChange,
-        );
-    }
   }
 
   startLoop(event?: ActionEvent) {