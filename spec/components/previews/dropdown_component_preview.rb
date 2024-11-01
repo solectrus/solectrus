1f8e1cb75ebf7ced050f5c7df576eaf6271258c2
Georg Ledermann <georg@ledermann.dev>
2024-11-01 11:29:58 +0100

    Fix previews

--- spec/components/previews/dropdown_component_preview.rb
+++ spec/components/previews/dropdown_component_preview.rb
@@ -1,18 +1,23 @@
 # @label Dropdown
 class DropdownComponentPreview < ViewComponent::Preview
   def default
-    render Dropdown::Component.new items: [
-                                     {
+    render Dropdown::Component.new name: 'sensor-selector',
+                                   items: [
+                                     MenuItem::Component.new(
                                        name: 'This is the first item',
                                        sensor: 'f1',
                                        href: '#',
-                                     },
-                                     {
+                                     ),
+                                     MenuItem::Component.new(
                                        name: 'Second',
                                        sensor: 'f2',
                                        href: '#',
-                                     },
-                                     { name: 'Third', sensor: 'f3', href: '#' },
+                                     ),
+                                     MenuItem::Component.new(
+                                       name: 'Third',
+                                       sensor: 'f3',
+                                       href: '#',
+                                     ),
                                    ],
                                    selected: 'f1'
   end
