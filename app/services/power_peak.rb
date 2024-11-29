443a60154d0df6b20a9fb2bd61aa9fb0ebbd1492
Georg Ledermann <georg@ledermann.dev>
2024-11-29 17:18:35 +0100

    PowerPeak: Fix handling single sensor

--- app/services/power_peak.rb
+++ app/services/power_peak.rb
@@ -11,12 +11,7 @@ class PowerPeak
     Rails
       .cache
       .fetch(['power_peak', existing_sensors], cache_options) do
-        Summary
-          .where(date: start..)
-          .calculate_all(
-            *existing_sensors.map { |sensor| :"max_max_#{sensor}" },
-          )
-          .tap { |result| break if result.values.compact.empty? }
+        calculate_peak(start)
       end
   end
 
@@ -25,6 +20,22 @@ class PowerPeak
   def existing_sensors
     @existing_sensors ||=
       sensors.select { |sensor| SensorConfig.x.exists?(sensor) }
+  end
+
+  def calculate_peak(start)
+    result =
+      Summary.where(date: start..).calculate_all(
+        *existing_sensors.map { |sensor| :"max_max_#{sensor}" },
+      )
+
+    # `calculate_all` returns a single value if only one sensor is provided
+    # Ensure the result is a hash with the sensor name as the key
+    if result.is_a?(Numeric) && existing_sensors.one?
+      return { existing_sensors.first => result }
+    end
+
+    # Return nil if all values are nil
+    result if result.values.any?
   end
 
   def cache_options
