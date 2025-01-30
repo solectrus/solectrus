165f86feab7dc9df6432827769b04f6f5a17f9f1
Georg Ledermann <georg@ledermann.dev>
2025-01-30 15:03:52 +0100

    Revert "Improve calculation of setup_id"
    This reverts commit 354a2b238d8cdb8b77e54ee8bf1e386eb2328e98.

--- spec/models/internal_metadata_spec.rb
+++
@@ -1,7 +0,0 @@
-describe InternalMetadata do
-  describe '.created_at' do
-    it 'returns the created_at timestamp of the first record' do
-      expect(described_class.created_at).to be_a(Time)
-    end
-  end
-end
