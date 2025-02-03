4fced4ab85d08db34c272f18c50fc6169dadaca1
Georg Ledermann <georg@ledermann.dev>
2025-02-03 18:54:52 +0100

    Indicator "approximate" not required anymore

--- app/components/splitted_costs/component.rb
+++ app/components/splitted_costs/component.rb
@@ -1,13 +1,12 @@
 class SplittedCosts::Component < ViewComponent::Base
-  def initialize(costs:, power_grid_ratio:, approximate: false)
+  def initialize(costs:, power_grid_ratio:)
     super()
     @costs = costs
 
     @power_grid_ratio = power_grid_ratio
-    @approximate = approximate
   end
 
-  attr_reader :costs, :power_grid_ratio, :approximate
+  attr_reader :costs, :power_grid_ratio
 
   def power_pv_ratio
     return unless power_grid_ratio
