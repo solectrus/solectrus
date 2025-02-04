47047c4c58b322a89239eeaa810f786af7ebb8a1
Georg Ledermann <georg@ledermann.dev>
2025-02-04 10:32:53 +0100

    Better variable naming

--- app/services/calculator/now.rb
+++ app/services/calculator/now.rb
@@ -1,39 +1,39 @@
 class Calculator::Now < Calculator::Base
   def initialize(sensors:)
     super()
-    @data = Flux::Last.new(sensors:).call
+    @last = Flux::Last.new(sensors:).call
   end
 
-  attr_reader :data
+  attr_reader :last
 
   # Timestamp of the data
 
   def time
-    data[:time]
+    last[:time]
   end
 
   ### System status
 
   def system_status
-    data[:system_status].to_utf8
+    last[:system_status].to_utf8
   end
 
   def system_status_ok
-    data[:system_status_ok]
+    last[:system_status_ok]
   end
 
   # Grid
 
   def grid_import_power
-    data[:grid_import_power]
+    last[:grid_import_power]
   end
 
   def grid_export_power
-    data[:grid_export_power]
+    last[:grid_export_power]
   end
 
   def grid_export_limit
-    data[:grid_export_limit]
+    last[:grid_export_limit]
   end
 
   def grid_export_limit_active?
@@ -45,25 +45,25 @@ class Calculator::Now < Calculator::Base
   # Battery
 
   def battery_charging_power
-    data[:battery_charging_power]
+    last[:battery_charging_power]
   end
 
   def battery_discharging_power
-    data[:battery_discharging_power]
+    last[:battery_discharging_power]
   end
 
   def battery_soc
-    data[:battery_soc]
+    last[:battery_soc]
   end
 
   def case_temp
-    data[:case_temp]
+    last[:case_temp]
   end
 
   # Inverter
 
   def inverter_power
-    data[:inverter_power]
+    last[:inverter_power]
   end
 
   # Consumer
@@ -73,33 +73,33 @@ class Calculator::Now < Calculator::Base
       SensorConfig
         .x
         .excluded_sensor_names
-        .reduce(data[:house_power].to_f) { |acc, elem| acc - data[elem].to_f },
+        .reduce(last[:house_power].to_f) { |acc, elem| acc - last[elem].to_f },
       0,
     ].max
   end
 
   def wallbox_power
-    data[:wallbox_power]
+    last[:wallbox_power]
   end
 
   def heatpump_power
-    data[:heatpump_power]
+    last[:heatpump_power]
   end
 
   # Custom consumers, define a method for each custom sensor
   SensorConfig::CUSTOM_SENSORS.each do |sensor_name|
     # Example:
     # def custom_power_01
-    define_method(sensor_name) { data[sensor_name] }
+    define_method(sensor_name) { last[sensor_name] }
   end
 
   # Car
 
   def car_battery_soc
-    data[:car_battery_soc]
+    last[:car_battery_soc]
   end
 
   def wallbox_car_connected
-    data[:wallbox_car_connected]
+    last[:wallbox_car_connected]
   end
 end
