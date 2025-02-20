400f23c8ee10da2effde822191c650574be84c13
Georg Ledermann <georg@ledermann.dev>
2025-02-20 06:35:45 +0100

    House: Split "other" chart, too
    Belongs to #3924

--- app/services/chart_data/house_power_without_custom.rb
+++ app/services/chart_data/house_power_without_custom.rb
@@ -9,61 +9,163 @@ class ChartData::HousePowerWithoutCustom < ChartData::Base
           {
             label: SensorConfig.x.name(chart_sensor),
             data: data.map(&:second),
-          }.merge(style)
+            stack: chart_sensor == :other_power ? nil : 'Power-Splitter',
+          }.merge(style(chart_sensor, split: chart.key?(:other_power_grid)))
         end,
     }
   end
 
   def chart
-    @chart ||=
-      begin
-        raw_chart =
-          PowerChart.new(sensors: [:house_power, *sensors_to_exclude]).call(
-            timeframe,
-          )
-
-        if raw_chart[:house_power].nil? || sensors_to_exclude.blank?
-          raw_chart
-        else
-          { house_power_without_custom: adjusted_house_power(raw_chart) }
-        end
-      end
+    @chart ||= splitting_allowed? ? splitted_chart : simple_chart
   end
 
-  def style
+  def simple_chart
+    raw_chart =
+      PowerChart.new(sensors: [:house_power, *excluded_sensor_names]).call(
+        timeframe,
+      )
+    if raw_chart[:house_power].nil? || excluded_sensor_names.blank?
+      return raw_chart
+    end
+
     {
-      fill: 'origin',
-      # Base color, will be changed to gradient in JS
-      backgroundColor: '#64748b', # bg-slate-500
-      borderWidth: 1,
-      borderRadius: 5,
-      borderSkipped: 'start',
+      other_power:
+        map_house_power(raw_chart) { |index| other_power(raw_chart, index) },
     }
   end
 
-  def adjusted_house_power(power_chart)
-    power_chart[:house_power]&.map&.with_index do |house_power, index|
-      # Exclude given sensors from house_power
-      timestamp, power = house_power
-      [
-        timestamp,
-        if power
-          [
-            0,
-            sensors_to_exclude.reduce(power) do |acc, elem|
-              acc - power_chart.dig(elem, index)&.second.to_f
-            end,
-          ].max
-        end,
-      ]
+  def splitted_chart
+    raw_chart =
+      PowerChart.new(
+        sensors: [
+          :house_power,
+          grid_sensor,
+          *excluded_sensor_names,
+          *included_grid_sensor_names,
+        ],
+      ).call(timeframe)
+
+    if raw_chart.key?(:house_power) && raw_chart.key?(grid_sensor)
+      {
+        other_power:
+          map_house_power(raw_chart) { |index| other_power(raw_chart, index) },
+        other_power_grid:
+          map_house_power(raw_chart) do |index|
+            other_power_grid(raw_chart, index)
+          end,
+        other_power_pv:
+          map_house_power(raw_chart) do |index|
+            other_power_pv(raw_chart, index)
+          end,
+      }
+    else
+      # No data for house_power_grid is present, return simple chart instead
+      {
+        other_power:
+          map_house_power(raw_chart) { |index| other_power(raw_chart, index) },
+      }
     end
   end
 
-  def sensors_to_exclude
-    @sensors_to_exclude ||=
-      (
-        SensorConfig.x.excluded_sensor_names +
-          SensorConfig.x.existing_custom_sensor_names
-      ).uniq
+  def map_house_power(raw_chart)
+    return [] unless raw_chart[:house_power]
+
+    raw_chart[:house_power].map.with_index do |(timestamp, _), index|
+      [timestamp, yield(index)]
+    end
+  end
+
+  def other_power(raw_chart, index)
+    house_power = raw_chart[:house_power][index]&.second
+    excluded_sensor_names
+      .reduce(house_power) do |acc, elem|
+        acc.to_f - raw_chart.dig(elem, index)&.second.to_f
+      end
+      .clamp(0, nil)
+  end
+
+  def other_power_grid(raw_chart, index)
+    house_power_grid = raw_chart[:house_power_grid][index]&.second
+    return 0 unless house_power_grid
+
+    custom_total_grid =
+      SensorConfig.x.included_custom_sensor_names.sum do |sensor|
+        raw_chart.dig("#{sensor}_grid", index)&.second.to_f
+      end
+
+    (house_power_grid - custom_total_grid).clamp(0, nil)
+  end
+
+  def other_power_pv(raw_chart, index)
+    other_power(raw_chart, index) - other_power_grid(raw_chart, index)
+  end
+
+  def excluded_sensor_names
+    SensorConfig.x.excluded_sensor_names +
+      SensorConfig.x.included_custom_sensor_names
+  end
+
+  def included_grid_sensor_names
+    SensorConfig.x.included_custom_sensor_names.map { "#{it}_grid" }
+  end
+
+  def background_color(chart_sensor)
+    {
+      other_power: '#64748b', # bg-slate-500
+      other_power_grid: '#dc2626', # bg-red-600
+      other_power_pv: '#16a34a', # bg-green-600
+    }[
+      chart_sensor
+    ]
+  end
+
+  def style(chart_sensor, split:)
+    if split
+      {
+        fill: 'origin',
+        # Base color, will be changed to gradient in JS
+        backgroundColor: background_color(chart_sensor),
+        barPercentage: chart_sensor == :other_power ? 0.7 : 1.3,
+        categoryPercentage: 0.7,
+        borderRadius:
+          if chart_sensor == :other_power
+            { topLeft: 5, bottomLeft: 0, topRight: 0, bottomRight: 0 }
+          else
+            0
+          end,
+        borderWidth:
+          case chart_sensor # rubocop:disable Style/HashLikeCase
+          when :other_power
+            { top: 1, left: 1 }
+          when :other_power_grid
+            { right: 1 }
+          when :other_power_pv
+            { top: 1, right: 1 }
+          end,
+        borderColor: background_color(chart_sensor),
+      }
+    else
+      {
+        fill: 'origin',
+        # Base color, will be changed to gradient in JS
+        backgroundColor: background_color(chart_sensor),
+        borderWidth: 1,
+        borderRadius: 5,
+        borderSkipped: 'start',
+      }
+    end
+  end
+
+  def grid_sensor
+    @grid_sensor ||=
+      (SensorConfig.x.single_consumer? ? :grid_import_power : :house_power_grid)
+  end
+
+  def splitting_allowed?
+    # As the data from the PowerSplitter is available in intervals only,
+    # we cannot use it for line diagrams.
+    return false if timeframe.short?
+
+    SensorConfig.x.exists?(grid_sensor)
   end
 end
