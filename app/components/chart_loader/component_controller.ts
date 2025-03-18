40d8c77aae2956a128633769997bb057961d72d1
Georg Ledermann <georg@ledermann.dev>
2025-03-18 15:59:47 +0100

    Chart: Render incomplete bar with hatch
    Belongs to #1931

--- app/components/chart_loader/component_controller.ts
+++ app/components/chart_loader/component_controller.ts
@@ -12,6 +12,7 @@ import {
   LineController,
   LinearScale,
   TimeScale,
+  TimeScaleOptions,
   Filler,
   Title,
   Tooltip,
@@ -86,6 +87,9 @@ export default class extends Controller<HTMLCanvasElement> {
   private maxValue: number = 0;
   private minValue: number = 0;
 
+  private chartData?: ChartData;
+  private chartOptions?: ChartOptions;
+
   connect() {
     this.process();
 
@@ -114,10 +118,12 @@ export default class extends Controller<HTMLCanvasElement> {
   }
 
   private process() {
-    const data = this.getData();
+    this.chartData = this.getData();
+    const data = this.chartData;
     if (!data) return;
 
-    const options = this.getOptions();
+    this.chartOptions = this.getOptions();
+    const options = this.chartOptions;
     if (!options) return;
 
     // Disable animation when user prefers reduced motion
@@ -351,11 +357,31 @@ export default class extends Controller<HTMLCanvasElement> {
       dataset.stack ? minAlpha : 1,
     );
 
-    // Replace background color with gradient
-    dataset.backgroundColor = (context: { chart: Chart; type: string }) => {
-      const { ctx, chartArea } = context.chart;
+    const hatchedBackgroundGradient = new ChartBackgroundGradient(
+      originalColor,
+      isNegative,
+      basePosition,
+      datasetExtent / extent,
+      minAlpha,
 
-      if (chartArea) return backgroundGradient.canvasGradient(ctx, chartArea);
+      // Stacked bar must not be gradiented, just use the given Alpha
+      dataset.stack ? minAlpha : 1,
+      true,
+    );
+
+    // Replace background color with gradient
+    dataset.backgroundColor = (context: {
+      chart: Chart;
+      type: string;
+      dataIndex: number;
+    }) => {
+      const { ctx, chartArea } = context.chart;
+      const index = context.dataIndex;
+
+      if (chartArea)
+        if (this.isIncomplete(index))
+          return hatchedBackgroundGradient.canvasGradient(ctx, chartArea);
+        else return backgroundGradient.canvasGradient(ctx, chartArea);
     };
 
     // Use original color for border
@@ -511,5 +537,37 @@ export default class extends Controller<HTMLCanvasElement> {
     if (firstAllNegative && secondAllPositive) return false;
 
     return true;
+  }
+
+  // Check if the bar with the given index contains incomplete data,
+  // i.e. it's the current day, week, month, or year
+  private isIncomplete(index: number): boolean {
+    if (!this.chartData?.labels) return false;
+    if (typeof this.chartData.labels[index] !== 'number') return false;
+
+    const dataDate = new Date(this.chartData.labels[index]);
+    const currentDate = new Date();
+
+    switch (this.timeUnit) {
+      case 'day':
+      case 'week':
+        return dataDate.toDateString() === currentDate.toDateString();
+
+      case 'month':
+        return (
+          dataDate.getMonth() === currentDate.getMonth() &&
+          dataDate.getFullYear() === currentDate.getFullYear()
+        );
+
+      case 'year':
+        return dataDate.getFullYear() === currentDate.getFullYear();
+
+      default:
+        return false;
+    }
+  }
+
+  private get timeUnit(): string | boolean {
+    return (this.chartOptions?.scales?.x as TimeScaleOptions).time?.unit;
   }
 }
