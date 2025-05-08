ad7bf51de11368115204bdf87c3fa26486dbca18
Georg Ledermann <georg@ledermann.dev>
2025-05-08 13:45:11 +0200

    Check services on /up

--- config/routes.rb
+++ config/routes.rb
@@ -69,7 +69,7 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get 'up' => 'rails/health#show', :as => :rails_health_check
+  get 'up' => 'health#show', :as => :health_check
   get 'skip-browser-check', to: 'application#skip_browser_check'
 
   mount Lookbook::Engine, at: '/lookbook' if Rails.env.development?
