6eee99053048e6043b2edf80814cc2cfae7e3569
Georg Ledermann <georg@ledermann.dev>
2025-05-14 13:29:50 +0200

    PowerPeak: Add inverter_power total when missing
    Belongs to #4310

--- app/services/power_peak.rb
+++ app/services/power_peak.rb
@@ -9,14 +9,23 @@ class PowerPeak
     return if existing_sensors.empty?
 
     start = start&.to_date
-    Rails
-      .cache
-      .fetch(cache_key(start), cache_options) do
-        query(start).symbolize_keys.presence
-      end
+    Rails.cache.fetch(cache_key(start), cache_options) { peaks(start:) }
   end
 
   private
+
+  def peaks(start:)
+    query(start).symbolize_keys.presence.tap do |raw|
+      if raw && SensorConfig.x.multi_inverter?
+        # Ensure there is a total inverter power value
+        raw[:inverter_power] ||= SensorConfig
+          .x
+          .existing_custom_inverter_sensor_names
+          .filter_map { raw[it] }
+          .sum # not totally correct, but close enough
+      end
+    end
+  end
 
   def existing_sensors
     @existing_sensors ||=
@@ -39,6 +48,6 @@ class PowerPeak
 
   def cache_key(start)
     sorted = existing_sensors.sort.join(',')
-    "power_peak:#{start}:#{Digest::SHA256.hexdigest(sorted)}"
+    "power_peak:v2:#{start}:#{Digest::SHA256.hexdigest(sorted)}"
   end
 end
