420aa2db9ec4faf23f45b5713cb6ab57a32743a0
Georg Ledermann <georg@ledermann.dev>
2025-06-22 08:06:24 +0200

    Chart: Fix missing inverter sensors
    Belongs to #4428

--- app/controllers/balance/charts_controller.rb
+++ app/controllers/balance/charts_controller.rb
@@ -15,16 +15,37 @@ class Balance::ChartsController < ApplicationController
   private
 
   def calculator_range
-    Calculator::Range.new(
-      timeframe,
-      calculations: [
-        Queries::Calculation.new(sensor, :sum, :sum),
+    Calculator::Range.new(timeframe, calculations:)
+  end
+
+  def calculations
+    if sensor == :inverter_power
+      [
+        *(
+          inverter_sensor_names.map do |sensor_name|
+            Queries::Calculation.new(sensor_name, :sum, :sum)
+          end
+        ),
         (
-          if timeframe.day? && sensor == :inverter_power
+          if timeframe.day?
             Queries::Calculation.new(:inverter_power_forecast, :sum, :sum)
           end
         ),
-      ].compact,
-    )
+      ].compact
+    else
+      [Queries::Calculation.new(sensor, :sum, :sum)]
+    end
+  end
+
+  def inverter_sensor_names
+    if multi_inverter_enabled?
+      ([:inverter_power] + SensorConfig.x.inverter_sensor_names).uniq
+    else
+      [:inverter_power]
+    end
+  end
+
+  def multi_inverter_enabled?
+    SensorConfig.x.multi_inverter? && ApplicationPolicy.multi_inverter?
   end
 end
