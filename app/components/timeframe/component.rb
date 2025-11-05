ae3db51b398db5db12ccfaf9d6abe2faf09b19fb
Georg Ledermann <georg@ledermann.dev>
2025-11-05 11:36:25 +0100

    Center timeframe display on mobile devices

--- app/components/timeframe/component.rb
+++ app/components/timeframe/component.rb
@@ -24,4 +24,11 @@ class Timeframe::Component < ViewComponent::Base
   def timeframe_select_path
     helpers.timeframe_select_path(sensor_name: helpers.sensor_name, timeframe:)
   end
+
+  def paginate_button_classes
+    'click-animation rounded-sm px-2 py-1 ' \
+      'hover:bg-indigo-500 hover:text-white ' \
+      'focus:ring-2 focus:ring-gray-300 focus:ring-offset-0 focus:outline-none ' \
+      'dark:hover:bg-indigo-800 dark:hover:text-gray-400 dark:focus:ring-gray-400'
+  end
 end
